8e8c20be564eb32e1e04c1c222d60ac991dd9f51
Erlend Vige <1488626+erlendev@users.noreply.github.com>
2022-10-13 15:05:02 +0200

    Made expand-component for paragraphs

--- src/components/søknad-detail-section/SøknadDetailSection.tsx
+++ src/components/søknad-detail-section/SøknadDetailSection.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Søknad from '../../types/Søknad';
 import { Alert, Heading } from '@navikt/ds-react';
+import ParagraphExpand from '../paragraph-expand/ParagraphExpand';
+import styles from './SøknadDetailSection.module.css';
 
 interface SøknadDetailSectionProps {
     søknad: Søknad;
@@ -8,7 +10,7 @@ interface SøknadDetailSectionProps {
 
 const SøknadDetailSection = (props: SøknadDetailSectionProps) => {
     return (
-        <div style={{ padding: '1rem' }}>
+        <div className={styles.søknadDetailSection}>
             <Heading level="1" size="small">
                 Søknad
             </Heading>
@@ -16,6 +18,14 @@ const SøknadDetailSection = (props: SøknadDetailSectionProps) => {
             <Alert variant="info" fullWidth style={{ width: '100%' }}>
                 Foreløpig har vi ikke alle opplysninger til å vurdere søknaden
             </Alert>
+            <div style={{ marginTop: '4rem' }}>
+                <ParagraphExpand title="Søknad om dagpenger (§7)">Test</ParagraphExpand>
+                <ParagraphExpand title="Statlige ytelser (§7)">Test</ParagraphExpand>
+                <ParagraphExpand title="Kommunale ytelser (§7)">Test</ParagraphExpand>
+                <ParagraphExpand title="Pensjonsordninger (§7)">Test</ParagraphExpand>
+                <ParagraphExpand title="Lønnsinntekt (§8)">Test</ParagraphExpand>
+                <ParagraphExpand title="Institusjon (§9)">Test</ParagraphExpand>
+            </div>
         </div>
     );
 };
