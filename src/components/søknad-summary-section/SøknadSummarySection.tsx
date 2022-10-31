3857923324c8067a4951e9410da700d186c67c2f
Erlend Vige <erlend.edvardsen.vige@nav.no>
2022-10-31 14:28:51 +0100

    Format dates in SøknadSummarySection

--- src/components/søknad-summary-section/SøknadSummarySection.tsx
+++ src/components/søknad-summary-section/SøknadSummarySection.tsx
@@ -4,6 +4,7 @@ import { Calender, FileContent, Office1, Refresh } from '@navikt/ds-icons';
 import Søknad from '../../types/Søknad';
 import styles from './SøknadSummarySection.module.css';
 import IconWithText from '../icon-with-text/IconWithText';
+import { formatDate } from '../../utils/date';
 
 interface SøknadSummarySectionProps {
     søknad: Søknad;
@@ -11,33 +12,35 @@ interface SøknadSummarySectionProps {
 
 const SøknadSummarySection = ({
     søknad: { søknadsdato, antallDager, startdato, sluttdato, arrangoernavn },
-}: SøknadSummarySectionProps) => (
-    <div className={styles.søknadSummarySection}>
-        <Heading size="small" level="1">
-            Oppsummering
-        </Heading>
-        <Heading className={styles.søknadHeading} size="xsmall" level="2">
-            Søknad
-        </Heading>
-        <div className={styles.søknadDetails}>
-            <IconWithText
-                className={styles.søknadDetail}
-                iconRenderer={() => <FileContent />}
-                text={`Søknadsdato: ${søknadsdato}`}
-            />
-            <IconWithText
-                className={styles.søknadDetail}
-                iconRenderer={() => <Calender />}
-                text={`${startdato} - ${sluttdato}`}
-            />
-            <IconWithText className={styles.søknadDetail} iconRenderer={() => <Office1 />} text={arrangoernavn} />
-            <IconWithText
-                className={styles.søknadDetail}
-                iconRenderer={() => <Refresh />}
-                text={`${antallDager} dager i uka (${(antallDager / 5.0) * 100.0}%)`}
-            />
+}: SøknadSummarySectionProps) => {
+    return (
+        <div className={styles.søknadSummarySection}>
+            <Heading size="small" level="1">
+                Oppsummering
+            </Heading>
+            <Heading className={styles.søknadHeading} size="xsmall" level="2">
+                Søknad
+            </Heading>
+            <div className={styles.søknadDetails}>
+                <IconWithText
+                    className={styles.søknadDetail}
+                    iconRenderer={() => <FileContent />}
+                    text={`Søknadsdato: ${formatDate(søknadsdato)}`}
+                />
+                <IconWithText
+                    className={styles.søknadDetail}
+                    iconRenderer={() => <Calender />}
+                    text={`${formatDate(startdato)} - ${formatDate(sluttdato)}`}
+                />
+                <IconWithText className={styles.søknadDetail} iconRenderer={() => <Office1 />} text={arrangoernavn} />
+                <IconWithText
+                    className={styles.søknadDetail}
+                    iconRenderer={() => <Refresh />}
+                    text={`${antallDager} dager i uka (${(antallDager / 5.0) * 100.0}%)`}
+                />
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default SøknadSummarySection;
