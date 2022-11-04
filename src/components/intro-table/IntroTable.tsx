62f42ee567a33445b4ce86158a038091db11d3b6
Erlend Vige <erlend.edvardsen.vige@nav.no>
2022-11-04 14:55:11 +0100

    Add periode to introduksjonsprogram-table
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>
    Co-authored-by: haagenhasle <Hagen.Hasle@nav.no>

--- src/components/intro-table/IntroTable.tsx
+++ src/components/intro-table/IntroTable.tsx
@@ -4,7 +4,8 @@ import { ErrorColored, SuccessColored, InformationColored } from '@navikt/ds-ico
 import styles from './IntroTable.module.css';
 import IconWithText from '../icon-with-text/IconWithText';
 import { Vilkårsvurdering } from '../../types/Søknad';
-import { formatPeriode, formatÅpenPeriode } from '../../utils/date';
+import { formatDate, formatPeriode, formatÅpenPeriode } from '../../utils/date';
+import { Periode, ÅpenPeriode } from '../../types/Periode';
 
 interface IntroTableProps {
     vilkårsvurderinger: Vilkårsvurdering[];
@@ -16,22 +17,34 @@ function renderIcon(utfall: string) {
     return <InformationColored />;
 }
 
+function renderPeriode(periode?: ÅpenPeriode) {
+    if (!periode) {
+        return '-';
+    }
+    if (!!periode.til) {
+        return formatPeriode(periode as Periode);
+    }
+    return formatDate(periode.fra);
+}
+
 const IntroTable = ({ vilkårsvurderinger }: IntroTableProps) => {
     return (
         <Table>
             <Table.Header>
                 <Table.Row>
                     <Table.HeaderCell>Registrering</Table.HeaderCell>
+                    <Table.HeaderCell>Periode</Table.HeaderCell>
                     <Table.HeaderCell>Kilde</Table.HeaderCell>
                     <Table.HeaderCell>Detaljer</Table.HeaderCell>
                 </Table.Row>
             </Table.Header>
             <Table.Body>
-                {vilkårsvurderinger.map(({ utfall, kilde, detaljer }, index) => (
+                {vilkårsvurderinger.map(({ utfall, kilde, detaljer, periode }, index) => (
                     <Table.Row key={`${utfall}${index}`}>
                         <Table.DataCell className={styles.registreringCell}>
                             <IconWithText iconRenderer={() => renderIcon(utfall)} text={utfall} />
                         </Table.DataCell>
+                        <Table.HeaderCell>{renderPeriode(periode)}</Table.HeaderCell>
                         <Table.DataCell>{kilde}</Table.DataCell>
                         <Table.DataCell>{detaljer}</Table.DataCell>
                     </Table.Row>
