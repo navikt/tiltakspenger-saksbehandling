9dea72ab6eb3ed38c7c5fdaae86abb42812b9a84
Erlend Vige <erlend.edvardsen.vige@nav.no>
2022-11-16 16:54:01 +0100

    Tester for auth.ts

--- src/utils/__tests__/auth.spec.ts
+++ src/utils/__tests__/auth.spec.ts
@@ -1,13 +1,52 @@
 import { getToken } from '../auth';
 
+global.fetch = jest.fn();
+
+const okResponse = {
+    ok: true,
+    json: () => Promise.resolve({ access_token: 'accessTokenMock' }),
+};
+
+const notOkResponse = {
+    status: 401,
+    headers: { 'content-type': 'application/json' },
+    body: null,
+};
+
+function getTokenCaller(params: any) {
+    return () => getToken(params);
+}
+
+function mockRequestWithAuthorizationHeader(authorizationHeaderValue?: string) {
+    return { headers: { authorization: authorizationHeaderValue } };
+}
+
 describe('getToken', () => {
     test('it should throw an error if no authorization header is provided', async () => {
-        const getTokenCaller = () => getToken({ headers: [] } as any);
-        expect(getTokenCaller).rejects.toThrow('Ingen tilgang');
+        const request = mockRequestWithAuthorizationHeader();
+        const getTokenCall = getTokenCaller(request);
+        expect(getTokenCall).rejects.toThrow('Ingen tilgang');
     });
 
     test('it should throw an error if the authorization header contains an invalid token', async () => {
-        const getTokenCaller = () => getToken({ headers: [{ authorization: 'et ugyldig token' }] } as any);
-        expect(getTokenCaller).rejects.toThrow('Ingen tilgang');
+        const request = mockRequestWithAuthorizationHeader('et ugyldig token');
+        const getTokenCall = getTokenCaller(request);
+        expect(getTokenCall).rejects.toThrow('Ingen tilgang');
+    });
+
+    test('it should reject with a response object when on-behalf-of grant fails', async () => {
+        // @ts-ignore
+        fetch.mockImplementationOnce(() => Promise.reject(notOkResponse));
+        const request = mockRequestWithAuthorizationHeader('Bearer blabla.blabla.blabla');
+        const getTokenCaller = () => getToken(request as any);
+        expect(getTokenCaller).rejects.toEqual(notOkResponse);
+    });
+
+    it('should resolve with the resulting access_token when on-behalf-of grant is successful', async () => {
+        // @ts-ignore
+        fetch.mockImplementationOnce(() => Promise.resolve(okResponse));
+        const request = mockRequestWithAuthorizationHeader('Bearer blabla.blabla.blabla');
+        const result = await getToken(request as any);
+        expect(result).toEqual('accessTokenMock');
     });
 });
