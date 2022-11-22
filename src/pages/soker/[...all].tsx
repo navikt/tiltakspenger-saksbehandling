f5975c1a2637e4a559d09780703335fd4178ce8a
Erlend Vige <erlend.edvardsen.vige@nav.no>
2022-11-22 16:50:26 +0100

    Fikser feil visning av feilmelding ved søk på behandling

--- src/pages/soker/[...all].tsx
+++ src/pages/soker/[...all].tsx
@@ -8,9 +8,9 @@ import SøknadSummarySection from '../../components/søknad-summary-section/Søk
 import PersonaliaHeader from '../../components/personalia-header/PersonaliaHeader';
 import { fetcher } from '../../utils/http';
 import { ApiError } from '../../types/Error';
-import styles from './Søker.module.css';
 import Søker from '../../types/Søker';
 import SøknadTabs from '../../components/søknad-tabs/SøknadTabs';
+import styles from './Søker.module.css';
 
 export const søknadIdAtom = atom('');
 
@@ -18,8 +18,20 @@ const SøkerPage: NextPage = () => {
     const router = useRouter();
     const urlParams = router.query.all;
     const [søkerId, søknadId] = urlParams as string[];
-    const { data: søkerResponse, error } = useSWR<Søker | ApiError>(`/api/person/soknader/${søkerId}`, fetcher);
     const [activeSøknadId, setActiveSøknadId] = useAtom(søknadIdAtom);
+
+    const { data: søkerResponse, error } = useSWR<Søker | ApiError>(`/api/person/soknader/${søkerId}`, fetcher);
+
+    function setDefaultActiveSøknadId() {
+        if (søknadId) {
+            setActiveSøknadId(søknadId);
+        } else if (søkerResponse) {
+            const behandlinger = (søkerResponse as Søker).behandlinger;
+            setActiveSøknadId(behandlinger[0].søknad.id);
+        }
+    }
+
+    React.useEffect(setDefaultActiveSøknadId, [søkerResponse]);
 
     function redirectToSøknadPage(id: string) {
         return router.push(`/soker/${søkerId}/${id}`);
@@ -40,31 +52,26 @@ const SøkerPage: NextPage = () => {
     if (søkerRequestHasFailed()) {
         return <div>{(søkerResponse as ApiError).error}</div>;
     }
+
     const søkerData = søkerResponse as Søker;
-    const defaultValgtSøknadId = søknadId || søkerData.behandlinger[0].søknad.id;
-    if (activeSøknadId === '') {
-        setActiveSøknadId(defaultValgtSøknadId);
-        return <div>Henter data om søknad</div>;
-    } else {
-        const valgtBehandling = søkerData.behandlinger.find((behandling) => behandling.søknad.id === activeSøknadId);
-        if (!valgtBehandling) {
-            return <Alert variant="error">Fant ikke behandlingen</Alert>;
-        }
-        return (
-            <React.Fragment>
-                <PersonaliaHeader personalia={søkerData.personopplysninger}></PersonaliaHeader>
-                <div className={styles.søknadWrapper}>
-                    <SøknadSummarySection søknadResponse={valgtBehandling} />
-                    <div className={styles.verticalLine}></div>
-                    <SøknadTabs
-                        onChange={(id) => redirectToSøknadPage(id)}
-                        defaultTab={defaultValgtSøknadId}
-                        behandlinger={søkerData.behandlinger}
-                    />
-                </div>
-            </React.Fragment>
-        );
+    const valgtBehandling = søkerData.behandlinger.find((behandling) => behandling.søknad.id === activeSøknadId);
+    if (!valgtBehandling) {
+        return <Alert variant="error">Fant ikke behandlingen</Alert>;
     }
+    return (
+        <React.Fragment>
+            <PersonaliaHeader personalia={søkerData.personopplysninger}></PersonaliaHeader>
+            <div className={styles.søknadWrapper}>
+                <SøknadSummarySection søknadResponse={valgtBehandling} />
+                <div className={styles.verticalLine}></div>
+                <SøknadTabs
+                    onChange={(id) => redirectToSøknadPage(id)}
+                    defaultTab={activeSøknadId}
+                    behandlinger={søkerData.behandlinger}
+                />
+            </div>
+        </React.Fragment>
+    );
 };
 
 export default SøkerPage;
