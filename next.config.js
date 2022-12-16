5f6d655e71d9a98d745bda63fa9bf5298ed79e05
Erlend Vige <erlend.edvardsen.vige@nav.no>
2022-12-16 16:51:40 +0100

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+let isDevelopment = process.env.NODE_ENV == 'development';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
     output: 'standalone',
@@ -6,6 +8,44 @@ module.exports = {
             {
                 source: '/saker/person',
                 destination: `https://tiltakspenger-vedtak.dev.intern.nav.no/saker/person`,
+            },
+        ];
+    },
+    async headers() {
+        const ContentSecurityPolicy = `
+          default-src 'self';
+          script-src 'self' ${isDevelopment ? "'unsafe-eval'" : ''};
+          style-src 'self' 'unsafe-inline';
+          font-src 'self' cdn.nav.no;
+        `;
+
+        const securityHeaders = [
+            {
+                key: 'Content-Security-Policy',
+                value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+            },
+            {
+                key: 'X-Frame-Options',
+                value: 'DENY',
+            },
+            {
+                key: 'X-XSS-Protection',
+                value: '1; mode=block',
+            },
+            {
+                key: 'X-Content-Type-Options',
+                value: 'nosniff',
+            },
+            {
+                key: 'Referrer-Policy',
+                value: 'no-referrer',
+            },
+        ];
+
+        return [
+            {
+                source: '/:path*',
+                headers: securityHeaders,
             },
         ];
     },
