6036c513ea8acf5c6e49e6dfab46fe0da93e901a
Steffen Lien <steffen.lien@mimedi.no>
2023-01-13 11:04:19 +0100

    endre på docker-oppsett for å teste støtte av auth mock

--- .nais/labs/mock/[...middleware].ts
+++ .nais/labs/mock/[...middleware].ts
@@ -24,28 +24,18 @@ async function makeApiRequest(request: NextApiRequest, oboToken: string): Promis
 }
 
 export async function middleware(request: NextApiRequest, response: NextApiResponse): Promise<void> {
-    let oboToken = null;
     try {
-        oboToken = await getToken(request);
-    } catch (error: any) {
-        const simpleResponse = error as SimpleResponse;
-        logger.error('Bruker har ikke tilgang, kall mot Azure feilet', error);
-        response.status(simpleResponse.status).json({ message: 'Bruker har ikke tilgang' });
-    }
-    if (oboToken) {
-        try {
-            const res = await makeApiRequest(request, oboToken as string);
-            if (res.ok) {
-                const body = await res.json();
-                response.status(res.status).json(body);
-            } else {
-                const error = await res.text();
-                response.status(res.status).json({ error: !error ? res.statusText : error });
-            }
-        } catch (error) {
-            logger.error('Fikk ikke kontakt med APIet, returnerer 502', error);
-            response.status(502).json({ message: 'Bad Gateway' });
+        const res = await makeApiRequest(request, 'token');
+        if (res.ok) {
+            const body = await res.json();
+            response.status(res.status).json(body);
+        } else {
+            const error = await res.text();
+            response.status(res.status).json({ error: !error ? res.statusText : error });
         }
+    } catch (error) {
+        logger.error('Fikk ikke kontakt med APIet, returnerer 502', error);
+        response.status(502).json({ message: 'Bad Gateway' });
     }
 }
 
