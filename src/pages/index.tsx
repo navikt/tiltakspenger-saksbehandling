4b6eefd858a39b0b1ca3b5538717c9d62c8a5469
Steffen Lien <steffen.lien@mimedi.no>
2023-02-01 13:29:48 +0100

    endre semantisk element

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
     return (
-        <div style={{ paddingLeft: '1rem' }}>
+        <main style={{ paddingLeft: '1rem' }}>
             <p>Start med å søke opp en person i søkefeltet.</p>
-        </div>
+        </main>
     );
 };
 
