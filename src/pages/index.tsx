24da96fd871c3410ad17cd604eed52a4525b17d0
Steffen Lien <steffen.lien@mimedi.no>
2023-02-01 11:26:19 +0100

    fjern ubrukt kode og fiks navngiving

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import type { NextPage } from 'next';
-import '@navikt/ds-css';
 
-type SøknadResponse = {
-    ident: string;
-    behandlinger: SøknadResponse[];
-};
-
-const Home: NextPage = () => {
+const HomePage: NextPage = () => {
     return (
         <div style={{ paddingLeft: '1rem' }}>
             <p>Start med å søke opp en person i søkefeltet.</p>
@@ -15,4 +9,4 @@ const Home: NextPage = () => {
     );
 };
 
-export default Home;
+export default HomePage;
