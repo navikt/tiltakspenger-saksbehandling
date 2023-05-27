fd1087d544852c0f0ab662fbe9ff0f18c7723a8b
Hågen Hasle <haagenhasle@gmail.com>
2023-05-27 20:21:23 +0200

    Endret vurdering-tekst når det kreves manuell vurdering

--- src/utils/vurderingText.ts
+++ src/utils/vurderingText.ts
@@ -1,12 +1,15 @@
-import { Vilkårsvurdering } from '../types/Søknad';
-import { Utfall } from '../types/Utfall';
+import {Vilkårsvurdering} from '../types/Søknad';
+import {Utfall} from '../types/Utfall';
 
-export default function createVurderingText({ utfall }: Vilkårsvurdering, ytelseText: string) {
+export default function createVurderingText({utfall}: Vilkårsvurdering, ytelseText: string) {
     if (Utfall.Oppfylt === utfall) {
         return `Bruker er ikke innvilget ${ytelseText}`;
     }
-    if (Utfall.IkkeOppfylt === utfall || Utfall.KreverManuellVurdering === utfall) {
+    if (Utfall.IkkeOppfylt === utfall) {
         return `Bruker er innvilget ${ytelseText}`;
+    }
+    if (Utfall.KreverManuellVurdering === utfall) {
+        return `Ukjent om bruker er innvilget ${ytelseText}`;
     }
     return '';
 }
