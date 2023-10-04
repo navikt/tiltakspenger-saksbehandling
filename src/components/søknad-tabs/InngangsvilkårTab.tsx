a73623269fe604929f210683cc8603d08211f10e
Benedicte Myrvoll <benemyr17@gmail.com>
2023-10-04 10:27:41 +0200

    Refaktorer Accordion og trekk den inn i SaksopplysningTable
    Co-authored-by: richardmartinsen <Richard.Martinsen@nav.no>
    Co-authored-by: erlendev <Erlend.Edvardsen.Vige@nav.no>
    Co-authored-by: thorellefsennav <Thor.Eirik.Ellefsen@nav.no>
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>

--- src/components/søknad-tabs/InngangsvilkårTab.tsx
+++ src/components/søknad-tabs/InngangsvilkårTab.tsx
@@ -1,96 +1,42 @@
-import {Alert, Button, DatePicker, Radio, RadioGroup, Select, useRangeDatepicker} from "@navikt/ds-react";
-import {AccordionItem} from "../vilkår-accordions/Accordion";
-import {SaksopplysningTable} from "../vilkår-accordions/SaksopplysningTable";
-import {SøknadLayout} from "../../layouts/soker/SøknadLayout";
-import React, {useState} from "react";
-import {Saksopplysning, SaksopplysningInnDTO} from "../../types/NyBehandling";
+import { Accordion, Alert, Button, DatePicker, Radio, RadioGroup, Select, useRangeDatepicker } from '@navikt/ds-react';
+import { SaksopplysningTable } from '../vilkår-accordions/SaksopplysningTable';
+import { SøknadLayout } from '../../layouts/soker/SøknadLayout';
+import React, { useState } from 'react';
+import { Saksopplysning, SaksopplysningInnDTO } from '../../types/NyBehandling';
+import styles from './Accordion.module.css';
 
 interface InngangsvilkårTabProps {
     saksopplysninger: SaksopplysningInnDTO[];
 }
 
-
-export const InngangsvilkårTab = ({saksopplysninger}: InngangsvilkårTabProps) => {
-    const [åpneRedigering, onÅpneRedigering] = useState<boolean>(false);
-
-    const handleChange = (val: any) => console.log(val);
-    const {datepickerProps, toInputProps, fromInputProps, selectedRange} = useRangeDatepicker({
-        fromDate: new Date('Sep 12 2023'),
-        onRangeChange: console.log,
-    });
-
+export const InngangsvilkårTab = ({ saksopplysninger }: InngangsvilkårTabProps) => {
     return (
         <SøknadLayout>
             <Alert variant="info">Det er noe greier her</Alert>
-            <div style={{padding: '1em'}}/>
-            {saksopplysninger.map(saksopplysning => {
-                return (
-                    <>
-                        <AccordionItem title={saksopplysning.vilkårTittel}>
-                            <SaksopplysningTable
-                                utfall={saksopplysning.utfall}
-                                fom={saksopplysning.fom}
-                                tom={saksopplysning.tom}
-                                kilde={saksopplysning.kilde}
-                                detaljer={saksopplysning.detaljer}
-                                fakta={saksopplysning.fakta}
-                                håndterStartRedigering={onÅpneRedigering}
-                            />
-                            {åpneRedigering && (
-                                <div
-                                    style={{
-                                        background: '#F2F3F5',
-                                        width: '100%',
-                                        height: '100%',
-                                        padding: '1rem',
-                                    }}
-                                >
-                                    <RadioGroup legend="Endre vilkår" onChange={(val: string) => handleChange(val)}>
-                                        <Radio value="Deltar ikke">Deltar ikke</Radio>
-                                        <Radio value="Deltar">Deltar</Radio>
-                                    </RadioGroup>
-                                    <div style={{padding: '1rem'}}/>
+            <div style={{ padding: '1em' }} />
 
-                                    <div
-                                        style={{
-                                            gap: '1rem',
-                                            paddingBottom: '0.5rem',
-                                        }}
-                                    >
-                                        <DatePicker {...datepickerProps}>
-                                            <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                                <DatePicker.Input {...fromInputProps} label="Fra"/>
-                                                <div style={{padding: '1rem'}}/>
-                                                <DatePicker.Input {...toInputProps} label="Til"/>
-                                            </div>
-                                        </DatePicker>
-                                    </div>
-                                    <div style={{padding: '0.5rem'}}/>
-                                    <Select label="Begrunnelse for endring" style={{width: '415px'}}>
-                                        <option value="">Velg grunn</option>
-                                        <option value="Bruker møtte ikke opp">Bruker møtte ikke opp</option>
-                                        <option value="Bruker vil ikke ha penger">Bruker vil ikke ha penger</option>
-                                    </Select>
-                                    <div style={{padding: '1rem'}}/>
-                                    <div>
-                                        <Button
-                                            onClick={() => onÅpneRedigering(false)}
-                                            variant="tertiary"
-                                            style={{marginRight: '2rem'}}
-                                        >
-                                            Avbryt
-                                        </Button>
-                                        <Button>Lagre endring</Button>
-                                    </div>
-                                </div>
-                            )}
-                        </AccordionItem>
-                        <div style={{marginTop: '0.5rem'}}/>
-                    </>
-                )
-            })}
-
-
+            <Accordion variant="neutral">
+                {saksopplysninger.map((saksopplysning) => {
+                    return (
+                        <>
+                            <Accordion.Item style={{ background: '#FFFFFF' }}>
+                                <Accordion.Header>{saksopplysning.vilkårTittel}</Accordion.Header>
+                                <Accordion.Content className={styles.accordionContent}>
+                                    <SaksopplysningTable
+                                        utfall={saksopplysning.utfall}
+                                        fom={saksopplysning.fom}
+                                        tom={saksopplysning.tom}
+                                        kilde={saksopplysning.kilde}
+                                        detaljer={saksopplysning.detaljer}
+                                        fakta={saksopplysning.fakta}
+                                    />
+                                </Accordion.Content>
+                            </Accordion.Item>
+                            <div style={{ marginTop: '0.5rem' }} />
+                        </>
+                    );
+                })}
+            </Accordion>
         </SøknadLayout>
-    )
-}
+    );
+};
--- src/components/vilkår-accordions/SaksopplysningTable.tsx
+++ src/components/vilkår-accordions/SaksopplysningTable.tsx
@@ -1,7 +1,7 @@
-import {BodyShort, Button, Table} from '@navikt/ds-react';
-import {UtfallIcon} from '../utfall-icon/UtfallIcon';
-import React from 'react';
-import {PencilIcon} from '@navikt/aksel-icons';
+import { BodyShort, Button, DatePicker, Radio, RadioGroup, Select, Table, useRangeDatepicker } from '@navikt/ds-react';
+import { UtfallIcon } from '../utfall-icon/UtfallIcon';
+import React, { useState } from 'react';
+import { PencilIcon } from '@navikt/aksel-icons';
 
 interface SaksopplysningProps {
     utfall: string;
@@ -11,58 +11,116 @@ interface SaksopplysningProps {
     kilde: string;
     detaljer: string;
     fakta: string;
-    håndterStartRedigering: (value: React.SetStateAction<boolean>) => void;
 }
 
-export const SaksopplysningTable = ({ utfall, fom, tom, kilde, detaljer, fakta, håndterStartRedigering }: SaksopplysningProps) => {
+export const SaksopplysningTable = ({ utfall, fom, tom, kilde, detaljer, fakta }: SaksopplysningProps) => {
+    const [åpneRedigering, onÅpneRedigering] = useState<boolean>(false);
+    const handleChange = (val: any) => console.log(val);
+
+    const { datepickerProps, toInputProps, fromInputProps, selectedRange } = useRangeDatepicker({
+        fromDate: new Date('Sep 12 2023'),
+        onRangeChange: console.log,
+    });
     return (
-        <Table>
-            <Table.Header>
-                <Table.Row>
-                    <Table.HeaderCell></Table.HeaderCell>
-                    <Table.HeaderCell>Registrering</Table.HeaderCell>
-                    <Table.HeaderCell>Periode</Table.HeaderCell>
-                    <Table.HeaderCell>Kilde</Table.HeaderCell>
-                    <Table.HeaderCell>Detaljer</Table.HeaderCell>
-                    <Table.HeaderCell />
-                </Table.Row>
-            </Table.Header>
-            <Table.Body>
-                <Table.Row>
-                    <Table.DataCell>
-                        {
-                            <div style={{ display: 'flex' }}>
-                                <UtfallIcon utfall={utfall} />
-                            </div>
-                        }
-                    </Table.DataCell>
-                    <Table.DataCell>
-                        {
-                            <div style={{ display: 'flex' }}>
-                                <BodyShort>{fakta}</BodyShort>
-                            </div>
-                        }
-                    </Table.DataCell>
-                    <Table.DataCell>
-                        <BodyShort>{fom && tom ? `${fom} - ${tom}` : '-'}</BodyShort>
-                    </Table.DataCell>
-                    <Table.DataCell>
-                        <BodyShort>{kilde ? kilde : '-'}</BodyShort>
-                    </Table.DataCell>
-                    <Table.DataCell>
-                        <BodyShort>{detaljer ? detaljer : '-'}</BodyShort>
-                    </Table.DataCell>
-                    <Table.DataCell>
-                        <Button
-                            onClick={() => håndterStartRedigering(true)}
-                            variant="tertiary"
-                            iconPosition="left"
-                            icon={<PencilIcon />}
-                            aria-label="hidden"
-                        />
-                    </Table.DataCell>
-                </Table.Row>
-            </Table.Body>
-        </Table>
+        <>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell></Table.HeaderCell>
+                        <Table.HeaderCell>Registrering</Table.HeaderCell>
+                        <Table.HeaderCell>Periode</Table.HeaderCell>
+                        <Table.HeaderCell>Kilde</Table.HeaderCell>
+                        <Table.HeaderCell>Detaljer</Table.HeaderCell>
+                        <Table.HeaderCell />
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    <Table.Row>
+                        <Table.DataCell>
+                            {
+                                <div style={{ display: 'flex' }}>
+                                    <UtfallIcon utfall={utfall} />
+                                </div>
+                            }
+                        </Table.DataCell>
+                        <Table.DataCell>
+                            {
+                                <div style={{ display: 'flex' }}>
+                                    <BodyShort>{fakta}</BodyShort>
+                                </div>
+                            }
+                        </Table.DataCell>
+                        <Table.DataCell>
+                            <BodyShort>{fom && tom ? `${fom} - ${tom}` : '-'}</BodyShort>
+                        </Table.DataCell>
+                        <Table.DataCell>
+                            <BodyShort>{kilde ? kilde : '-'}</BodyShort>
+                        </Table.DataCell>
+                        <Table.DataCell>
+                            <BodyShort>{detaljer ? detaljer : '-'}</BodyShort>
+                        </Table.DataCell>
+                        <Table.DataCell>
+                            <Button
+                                onClick={() => onÅpneRedigering(!åpneRedigering)}
+                                variant="tertiary"
+                                iconPosition="left"
+                                icon={<PencilIcon />}
+                                aria-label="hidden"
+                            />
+                        </Table.DataCell>
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+            <>
+                {åpneRedigering && (
+                    <div
+                        style={{
+                            background: '#F2F3F5',
+                            width: '100%',
+                            height: '100%',
+                            padding: '1rem',
+                        }}
+                    >
+                        <RadioGroup legend="Endre vilkår" onChange={(val: string) => handleChange(val)}>
+                            <Radio value="Deltar ikke">Deltar ikke</Radio>
+                            <Radio value="Deltar">Deltar</Radio>
+                        </RadioGroup>
+                        <div style={{ padding: '1rem' }} />
+
+                        <div
+                            style={{
+                                gap: '1rem',
+                                paddingBottom: '0.5rem',
+                            }}
+                        >
+                            <DatePicker {...datepickerProps}>
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                                    <DatePicker.Input {...fromInputProps} label="Fra" />
+                                    <div style={{ padding: '1rem' }} />
+                                    <DatePicker.Input {...toInputProps} label="Til" />
+                                </div>
+                            </DatePicker>
+                        </div>
+                        <div style={{ padding: '0.5rem' }} />
+                        <Select label="Begrunnelse for endring" style={{ width: '415px' }}>
+                            <option value="">Velg grunn</option>
+                            <option value="Bruker møtte ikke opp">Bruker møtte ikke opp</option>
+                            <option value="Bruker vil ikke ha penger">Bruker vil ikke ha penger</option>
+                        </Select>
+                        <div style={{ padding: '1rem' }} />
+                        <div>
+                            <Button
+                                onClick={() => onÅpneRedigering(false)}
+                                variant="tertiary"
+                                style={{ marginRight: '2rem' }}
+                            >
+                                Avbryt
+                            </Button>
+                            <Button>Lagre endring</Button>
+                        </div>
+                    </div>
+                )}
+            </>
+        </>
     );
 };
