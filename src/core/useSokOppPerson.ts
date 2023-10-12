5dc177642b9fe1805840022a3c7497d11ef00f96
Thor Eirik Ellefsen <thor.eirik.ellefsen@nav.no>
2023-10-12 11:46:06 +0200

    Fikser typescriptfeil etter versjonsendring av swr (breaking changes)

--- src/core/useSokOppPerson.ts
+++ src/core/useSokOppPerson.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import useSWR from 'swr';
 import { Behandling } from '../types/Behandling';
-import Søker, { SøkerResponse } from '../types/Søker';
+import Søker, {SøkerIdent, SøkerResponse} from '../types/Søker';
 import { fetcher, FetcherError, fetchSøker } from '../utils/http';
 import toast from 'react-hot-toast';
 import { Saksbehandler } from '../types/Saksbehandler';
@@ -10,7 +10,7 @@ import { useRouter } from 'next/router';
 
 function useSokOppPerson(navigateToSoker: boolean = true) {
     const router = useRouter();
-    const { trigger, isMutating: isSokerMutating } = useSWRMutation<SøkerResponse>('/api/soker', fetchSøker, {
+    const { trigger, isMutating: isSokerMutating } = useSWRMutation<SøkerResponse, FetcherError, "/api/soker", SøkerIdent>('/api/soker', fetchSøker, {
         onSuccess: (data) => navigateToSoker && router.push(`/soker/${data.id}`),
         onError: (error: FetcherError) => toast.error(`[${error.status}] ${error.info}`),
     });
