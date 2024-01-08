caf7be1faa9137c243e460f8c0c9fae72153525a
Benedicte Myrvoll <benemyr17@gmail.com>
2024-01-08 14:23:08 +0100

    R Fjern ubrukt dependency

--- src/pages/behandling/[...all].tsx
+++ src/pages/behandling/[...all].tsx
@@ -1,7 +1,6 @@
 import React, { useContext, useRef, useState } from 'react';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { atom } from 'jotai';
 import SøknadSummarySection from '../../containers/søknad-summary-section/SøknadSummarySection';
 import PersonaliaHeader from '../../containers/personalia-header/PersonaliaHeader';
 import BehandlingTabs from '../../containers/søknad-tabs/BehandlingTabs';
@@ -18,8 +17,6 @@ import BegrunnelseModal from '../../containers/begrunnelse-modal/BegrunnelseModa
 import { Saksdialog } from '../../containers/saksdialog/Saksdialog';
 import { Detaljer } from '../../containers/meldekort-detaljer/meldekort-detaljer';
 import { MeldekortListe } from '../../containers/meldekort-liste/meldekort-liste';
-
-export const søknadIdAtom = atom('');
 
 const BehandlingPage: NextPage = () => {
   const router = useRouter();
