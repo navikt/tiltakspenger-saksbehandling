f84bd60b23ebfb23c81b59b17cc8decc333761a8
karlevaldnav <karl.evald.wigestrand@nav.no>
2024-03-07 13:34:27 +0100

    R Fjern ubrukt kode som har med Utfall å gjøre
    Co-authored-by: erlendev <Erlend.Edvardsen.Vige@nav.no>

--- src/types/Søknad.ts
+++ src/types/Søknad.ts
@@ -1,5 +1,4 @@
-import { Periode, ÅpenPeriode } from './Periode';
-import { Utfall } from './Utfall';
+import { Periode } from './Periode';
 import Tiltaksstatus from './Tiltaksstatus';
 
 export type RegistrertTiltak = {
@@ -17,30 +16,6 @@ interface Søknad {
   tiltakstype: string;
   deltakelseFom: string;
   deltakelseTom: string;
-}
-
-export interface Vilkårsvurdering {
-  kilde: string;
-  detaljer: string;
-  periode: ÅpenPeriode;
-  kreverManuellVurdering: boolean;
-  utfall: Utfall;
-}
-
-export interface Barnetillegg {
-  alder: number;
-  bosatt: string;
-  kilde: string;
-  navn: string;
-  fødselsdato: string;
-  søktBarnetillegg: boolean;
-  utfall: Utfall;
-}
-
-export interface Vedlegg {
-  dokumentInfoId: string;
-  filnavn: string;
-  journalpostId: string;
 }
 
 export default Søknad;
