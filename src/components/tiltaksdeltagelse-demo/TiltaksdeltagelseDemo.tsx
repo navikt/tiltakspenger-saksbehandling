5d55d3bc0d7586dd43517ddbaf5cbaf0aea04e8b
Erlend Vige <erlend.edvardsen.vige@nav.no>
2024-04-24 10:31:18 +0200

    F Lagt til form for tiltaksdeltagelse

--- src/components/tiltaksdeltagelse-demo/TiltaksdeltagelseDemo.tsx
+++ src/components/tiltaksdeltagelse-demo/TiltaksdeltagelseDemo.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { BodyShort, Button, Heading, List } from '@navikt/ds-react';
+import {
+  BodyShort,
+  Button,
+  Heading,
+  List,
+  Select,
+  Table,
+} from '@navikt/ds-react';
 import styles from './TiltaksdeltagelseDemo.module.css';
 import { Periode } from '../../types/Periode';
 import { formatPeriode } from '../../utils/date';
 import { PencilIcon } from '@navikt/aksel-icons';
+import Periodevelger from '../saksopplysning-tabell/PeriodeVelger';
 
 const TiltaksdeltagelseDemo = () => {
   return (
@@ -12,16 +20,18 @@ const TiltaksdeltagelseDemo = () => {
       <div className={styles.tiltakCardWrapper}>
         <TiltakCard
           tittel="Tiltakstype - Testarrangør"
-          periode={{ fra: '2026-01-01', til: '2026-01-05' }}
+          periode={{ fra: '2026-01-01', til: '2026-05-01' }}
           status="Gjennomføres"
           deltagelser={[
             {
               periode: { fra: '2026-01-01', til: '2026-01-31' },
               antallDagerIUken: 5,
+              status: 'Gjennomføres',
             },
             {
-              periode: { fra: '2026-01-02', til: '2026-01-05' },
+              periode: { fra: '2026-02-01', til: '2026-05-01' },
               antallDagerIUken: 3,
+              status: 'Gjennomføres',
             },
           ]}
         />
@@ -33,6 +43,7 @@ const TiltaksdeltagelseDemo = () => {
 interface Tiltaksdeltagelse {
   periode: Periode;
   antallDagerIUken: number;
+  status: String;
 }
 
 interface TiltakCardProps {
@@ -51,37 +62,89 @@ const TiltakCard = ({
   const [editMode, setEditMode] = React.useState(false);
   React.useEffect(() => console.log(`editMode: ${editMode}`), [editMode]);
   return (
-    <div className={styles.tiltakCard}>
-      <Heading size="medium">{tittel}</Heading>
-      <div className={styles.tiltakCard__ingress}>
-        <BodyShort>
-          <b>Vurderingsperiode: </b>
-          {formatPeriode(periode)}
-        </BodyShort>
-        <BodyShort>
-          <b>Status: </b>
-          {status}
-        </BodyShort>
+    <div style={{ maxWidth: '800px' }}>
+      <div className={styles.tiltakCard}>
+        <Heading size="medium">{tittel}</Heading>
+        <div className={styles.tiltakCard__ingress}>
+          <BodyShort>
+            <b>Vurderingsperiode: </b>
+            {formatPeriode(periode)}
+          </BodyShort>
+          <BodyShort>
+            <b>Status: </b>
+            {status}
+          </BodyShort>
+        </div>
+        <div className={styles.tiltakCard__deltagelseSection}>
+          <Table size="small">
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell scope="col">Periode</Table.HeaderCell>
+                <Table.HeaderCell scope="col">
+                  Antall dager per uke
+                </Table.HeaderCell>
+                <Table.HeaderCell scope="col">Status</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {deltagelser.map(({ periode, antallDagerIUken, status }) => {
+                const formattertPeriode = formatPeriode(periode);
+                return (
+                  <Table.Row key={formattertPeriode}>
+                    <Table.DataCell>{formattertPeriode}</Table.DataCell>
+                    <Table.DataCell>{antallDagerIUken}</Table.DataCell>
+                    <Table.DataCell>{status}</Table.DataCell>
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table>
+        </div>
+        <Button
+          icon={<PencilIcon title="Rediger" />}
+          variant={'tertiary'}
+          className={styles.tiltakCard__iconButton}
+          type="button"
+          onClick={() => setEditMode(!editMode)}
+        />
       </div>
-      <div className={styles.tiltakCard__deltagelseSection}>
-        <List title="Deltagelse">
-          {deltagelser.map(({ periode, antallDagerIUken }) => {
-            const formattertPeriode = formatPeriode(periode);
-            return (
-              <List.Item key={formattertPeriode}>
-                {formattertPeriode} ({antallDagerIUken} dager i uken)
-              </List.Item>
-            );
-          })}
-        </List>
-      </div>
-      <Button
-        icon={<PencilIcon title="Rediger" />}
-        variant={'tertiary'}
-        className={styles.tiltakCard__iconButton}
-        type="button"
-        onClick={() => setEditMode(!editMode)}
-      />
+      {editMode && (
+        <div className={styles.tiltakCard__form}>
+          <form>
+            <fieldset className={styles.tiltakCard__form__fieldset}>
+              <Periodevelger
+                onFromChange={() => console.log('from')}
+                onToChange={() => console.log('til')}
+                size="small"
+              />
+              <Select
+                label="Antall dager"
+                className={styles.tiltakCard__form__antallDagerSelectBox}
+                size="small"
+              >
+                <option value={1}>1</option>
+                <option value={2}>2</option>
+                <option value={3}>3</option>
+                <option value={4}>4</option>
+                <option value={5}>5</option>
+              </Select>
+            </fieldset>
+            <div className={styles.tiltakCard__form__buttonSection}>
+              <Button size="small" type="button">
+                Lagre
+              </Button>
+              <Button
+                variant="tertiary"
+                size="small"
+                type="reset"
+                onClick={() => setEditMode(false)}
+              >
+                Avbryt
+              </Button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
