7077b8533bba84af45560d6fb46007917447a568
Erlend Vige <erlend.edvardsen.vige@nav.no>
2024-05-24 14:59:03 +0200

    B Fikset state-bug i VilkårsvurderingAvStønadsdager.tsx
    Co-authored-by: KarlEvaldNav <Karl.Evald.Wigestrand@nav.no>

--- src/components/vilkårsvurdering-av-stønadsdager/VilkårsvurderingAvStønadsdager.tsx
+++ src/components/vilkårsvurdering-av-stønadsdager/VilkårsvurderingAvStønadsdager.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import {
-  BodyShort,
-  Box,
-  Button,
-  ExpansionCard,
-  HStack,
-  Link,
-} from '@navikt/ds-react';
+import { BodyShort, Box, Button } from '@navikt/ds-react';
 import { formatPeriode } from '../../utils/date';
 import TiltaksdeltagelseTable from '../tiltaksdeltagelse-table/TiltaksdeltagelseTable';
 import { PencilIcon } from '@navikt/aksel-icons';
@@ -15,97 +8,71 @@ import { RegistrertTiltak } from '../../types/Søknad';
 import styles from './VilkårsvurderingAvStønadsdager.module.css';
 
 interface VilkårsvurderingAvStønadsdagerProps {
-  registrerteTiltak: RegistrertTiltak[];
+  registrertTiltak: RegistrertTiltak;
 }
 
 const VilkårsvurderingAvStønadsdager = ({
-  registrerteTiltak,
+  registrertTiltak,
 }: VilkårsvurderingAvStønadsdagerProps) => {
   const [editMode, setEditMode] = React.useState(false);
+  const { periode, navn, arrangør, dagerIUken, kilde } = registrertTiltak;
   return (
-    <div className={styles.container}>
-      <ExpansionCard aria-label="Stønadsdager">
-        <ExpansionCard.Header>
-          <HStack wrap={false} gap="4" align="center">
-            <div>
-              <ExpansionCard.Title>Stønadsdager</ExpansionCard.Title>
-              <ExpansionCard.Description>
-                <Link
-                  href="https://lovdata.no/dokument/SF/forskrift/2013-11-04-1286"
-                  target="_blank"
-                >
-                  Tiltakspengeforskriften § 6-1 Stønadsdager
-                </Link>
-              </ExpansionCard.Description>
-            </div>
-          </HStack>
-        </ExpansionCard.Header>
-        <ExpansionCard.Content>
-          {registrerteTiltak.map(
-            ({ navn, dagerIUken, arrangør, periode, kilde }) => {
-              return (
-                <div key={formatPeriode(periode)} style={{ marginTop: '1rem' }}>
-                  <Box
-                    background="surface-subtle"
-                    padding="2"
-                    style={{ position: 'relative' }}
-                  >
-                    <BodyShort size="medium" spacing>
-                      <b>Tiltaksvariant:</b> {navn}
-                    </BodyShort>
-                    <BodyShort size="medium" spacing>
-                      <b>Arrangør:</b> {arrangør}
-                    </BodyShort>
-                    <BodyShort size="medium" spacing>
-                      <b>Tiltaksperiode:</b> {formatPeriode(periode)}
-                    </BodyShort>
-                    <BodyShort size="medium" spacing>
-                      <b>Antall dager registrert på tiltaket:</b> {dagerIUken}
-                    </BodyShort>
-                    <BodyShort size="medium" spacing>
-                      <b>Kilde:</b> {kilde}
-                    </BodyShort>
-                    {
-                      <TiltaksdeltagelseTable
-                        deltagelsesperioder={[
-                          {
-                            periode: periode,
-                            antallDager: dagerIUken,
-                            status: 'test',
-                          },
-                        ]}
-                      />
-                    }
-                    <div style={{ position: 'absolute', right: 0, top: 0 }}>
-                      <Button
-                        icon={<PencilIcon title="Rediger" />}
-                        variant={'tertiary'}
-                        className={styles.tiltakCard__iconButton}
-                        type="button"
-                        onClick={() => setEditMode(!editMode)}
-                      >
-                        Rediger
-                      </Button>
-                    </div>
-                    {editMode && (
-                      <div className={styles.tiltakCard__form}>
-                        <TiltaksdeltagelseForm
-                          onSubmit={() => {
-                            setEditMode(false);
-                          }}
-                          onCancel={() => {
-                            setEditMode(false);
-                          }}
-                        />
-                      </div>
-                    )}
-                  </Box>
-                </div>
-              );
-            },
-          )}
-        </ExpansionCard.Content>
-      </ExpansionCard>
+    <div key={formatPeriode(periode)} style={{ marginTop: '1rem' }}>
+      <Box
+        background="surface-subtle"
+        padding="2"
+        style={{ position: 'relative' }}
+      >
+        <BodyShort size="medium" spacing>
+          <b>Tiltaksvariant:</b> {navn}
+        </BodyShort>
+        <BodyShort size="medium" spacing>
+          <b>Arrangør:</b> {arrangør}
+        </BodyShort>
+        <BodyShort size="medium" spacing>
+          <b>Tiltaksperiode:</b> {formatPeriode(periode)}
+        </BodyShort>
+        <BodyShort size="medium" spacing>
+          <b>Antall dager registrert på tiltaket:</b> {dagerIUken}
+        </BodyShort>
+        <BodyShort size="medium" spacing>
+          <b>Kilde:</b> {kilde}
+        </BodyShort>
+        {
+          <TiltaksdeltagelseTable
+            deltagelsesperioder={[
+              {
+                periode: periode,
+                antallDager: dagerIUken,
+                status: 'test',
+              },
+            ]}
+          />
+        }
+        <div style={{ position: 'absolute', right: 0, top: 0 }}>
+          <Button
+            icon={<PencilIcon title="Rediger" />}
+            variant={'tertiary'}
+            className={styles.tiltakCard__iconButton}
+            type="button"
+            onClick={() => setEditMode(!editMode)}
+          >
+            Rediger
+          </Button>
+        </div>
+        {editMode && (
+          <div className={styles.tiltakCard__form}>
+            <TiltaksdeltagelseForm
+              onSubmit={() => {
+                setEditMode(false);
+              }}
+              onCancel={() => {
+                setEditMode(false);
+              }}
+            />
+          </div>
+        )}
+      </Box>
     </div>
   );
 };
--- src/pages/behandling/[behandlingId]/ovrigevilkar/index.tsx
+++ src/pages/behandling/[behandlingId]/ovrigevilkar/index.tsx
@@ -1,14 +1,15 @@
+import React, { ReactElement } from 'react';
 import { pageWithAuthentication } from '../../../../utils/pageWithAuthentication';
-import { Loader } from '@navikt/ds-react';
+import { ExpansionCard, HStack, Link, Loader } from '@navikt/ds-react';
 import { useRouter } from 'next/router';
 import { useHentBehandling } from '../../../../hooks/useHentBehandling';
 import { BehandlingLayout } from '../../../../components/layout/BehandlingLayout';
 import { SaksbehandlingLayout } from '../../../../components/layout/SaksbehandlingLayout';
-import { ReactElement } from 'react';
 import { NextPageWithLayout } from '../../../_app';
 import SøknadOppsummering from '../../../../components/søknad-oppsummering/SøknadOppsummering';
 import { Saksdialog } from '../../../../components/saksdialog/Saksdialog';
 import VilkårsvurderingAvStønadsdager from '../../../../components/vilkårsvurdering-av-stønadsdager/VilkårsvurderingAvStønadsdager';
+import styles from '../../../../components/vilkårsvurdering-av-stønadsdager/VilkårsvurderingAvStønadsdager.module.css';
 
 const Behandling: NextPageWithLayout = () => {
   const router = useRouter();
@@ -25,9 +26,33 @@ const Behandling: NextPageWithLayout = () => {
         søknad={valgtBehandling.søknad}
         registrerteTiltak={valgtBehandling.registrerteTiltak}
       />
-      <VilkårsvurderingAvStønadsdager
-        registrerteTiltak={valgtBehandling.registrerteTiltak}
-      />
+      <div className={styles.container}>
+        <ExpansionCard aria-label="Stønadsdager">
+          <ExpansionCard.Header>
+            <HStack wrap={false} gap="4" align="center">
+              <div>
+                <ExpansionCard.Title>Stønadsdager</ExpansionCard.Title>
+                <ExpansionCard.Description>
+                  <Link
+                    href="https://lovdata.no/dokument/SF/forskrift/2013-11-04-1286"
+                    target="_blank"
+                  >
+                    Tiltakspengeforskriften § 6-1 Stønadsdager
+                  </Link>
+                </ExpansionCard.Description>
+              </div>
+            </HStack>
+          </ExpansionCard.Header>
+          <ExpansionCard.Content>
+            {valgtBehandling.registrerteTiltak.map((registrertTiltak, i) => (
+              <VilkårsvurderingAvStønadsdager
+                registrertTiltak={registrertTiltak}
+                key={i}
+              />
+            ))}
+          </ExpansionCard.Content>
+        </ExpansionCard>
+      </div>
       <Saksdialog endringslogg={valgtBehandling.endringslogg} />
     </>
   );
