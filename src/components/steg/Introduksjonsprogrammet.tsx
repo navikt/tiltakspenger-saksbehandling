0cbb74b0f8e64517b2fb2ce7d7e41b3d794ee6a6
karlevaldnav <karl.evald.wigestrand@nav.no>
2024-07-18 09:06:02 +0200

    F Legg til kobling mot nytt Introduksjonsprogrammet-endepunkt i backend

--- src/components/steg/Introduksjonsprogrammet.tsx
+++ src/components/steg/Introduksjonsprogrammet.tsx
@@ -1,82 +1,101 @@
 import { Loader, VStack } from '@navikt/ds-react';
 import { useRouter } from 'next/router';
-import { useHentBehandling } from '../../hooks/useHentBehandling';
 import StegHeader from './StegHeader';
 import StegKort from './StegKort';
 import UtfallstekstMedIkon from './UtfallstekstMedIkon';
+import { dateTilISOTekst, nyPeriodeTilPeriode } from '../../utils/date';
 import { SkjemaFelter } from './OppdaterSaksopplysningForm';
 import { useSWRConfig } from 'swr';
-import { dateTilISOTekst } from '../../utils/date';
+import { useHentIntroduksjonsprogrammet } from '../../hooks/useHentIntroduksjonsprogrammet';
+import { Deltagelse } from '../../types/Kvp';
 
-const Introduksjonsprogrammet = () => {
+const Kvalifiseringsprogrammet = () => {
   const router = useRouter();
   const behandlingId = router.query.behandlingId as string;
-  const { valgtBehandling, isLoading } = useHentBehandling(behandlingId);
+  const { intro, isLoading } = useHentIntroduksjonsprogrammet(behandlingId);
   const mutator = useSWRConfig().mutate;
 
-  if (isLoading || !valgtBehandling) {
+  if (isLoading || !intro) {
     return <Loader />;
   }
 
-  const intro = valgtBehandling.ytelsessaksopplysninger.saksopplysninger.find(
-    (saksopplysning) =>
-      saksopplysning.saksopplysningTittel == 'Introduksjonsprogrammet',
-  );
+  const håndterLagreIntroSaksopplysning = (data: SkjemaFelter) => {
+    const deltakelseMedPeriode = {
+      periode: data.valgtVerdi
+        ? {
+          fraOgMed: dateTilISOTekst(data.periode.fra),
+          tilOgMed: dateTilISOTekst(data.periode.til),
+        }
+        : {
+          fraOgMed: vurderingsPeriode.fra,
+          tilOgMed: vurderingsPeriode.til,
+        },
+      deltar: data.valgtVerdi,
+    };
 
-  if (!intro) return <Loader />;
+    const årsakTilEndring = data.begrunnelse;
 
-  const håndterLagreSaksopplysning = (data: SkjemaFelter) => {
-    fetch(`/api/behandling/${behandlingId}`, {
+    fetch(`/api/behandling/${behandlingId}/vilkar/introduksjonsprogrammet`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        fra: data.valgtVerdi
-          ? dateTilISOTekst(data.periode?.fra)
-          : valgtBehandling.vurderingsperiode.fra,
-        til: data.valgtVerdi
-          ? dateTilISOTekst(data.periode?.til)
-          : valgtBehandling.vurderingsperiode.til,
-        vilkår: 'INTROPROGRAMMET',
-        begrunnelse: data.begrunnelse,
-        harYtelse: data.valgtVerdi,
+        ytelseForPeriode: [deltakelseMedPeriode],
+        årsakTilEndring: årsakTilEndring,
       }),
-    }).then(() => {
-      mutator(`/api/behandling/${behandlingId}`);
-    });
+    })
+      .then(() => {
+        mutator(`/api/behandling/${behandlingId}/vilkar/introduksjonsprogrammet`);
+      })
+      .catch((error) => {
+        throw new Error(
+          `Ikke implementert enda: ${error.message}`,
+        );
+      });
   };
 
+  const deltagelse = intro.avklartSaksopplysning.periodeMedDeltagelse.deltagelse;
+  const vurderingsPeriode = nyPeriodeTilPeriode(intro.vurderingsperiode);
+  const saksopplysningsPeriode = nyPeriodeTilPeriode(
+    intro.avklartSaksopplysning.periodeMedDeltagelse.periode ??
+    intro.søknadSaksopplysning.periodeMedDeltagelse.periode,
+  );
   return (
     <VStack gap="4">
       <StegHeader
         headertekst={'Introduksjonsprogrammet'}
-        lovdatatekst={
-          valgtBehandling.ytelsessaksopplysninger.vilkårLovreferanse.beskrivelse
-        }
-        paragraf={
-          valgtBehandling.ytelsessaksopplysninger.vilkårLovreferanse.paragraf
-        }
+        lovdatatekst={intro.vilkårLovreferanse.beskrivelse}
+        paragraf={intro.vilkårLovreferanse.paragraf}
         lovdatalenke={
           'https://lovdata.no/dokument/SF/forskrift/2013-11-04-1286'
         }
       />
-      <UtfallstekstMedIkon samletUtfall={intro.utfall} />
+      <UtfallstekstMedIkon samletUtfall={intro.samletUtfall} />
       <StegKort
         håndterLagreSaksopplysning={(data: SkjemaFelter) =>
-          håndterLagreSaksopplysning(data)
+          håndterLagreIntroSaksopplysning(data)
         }
         editerbar={false}
-        vurderingsperiode={valgtBehandling.vurderingsperiode}
-        saksopplysningsperiode={valgtBehandling.vurderingsperiode}
-        kilde={intro.kilde}
-        utfall={intro.utfall}
-        vilkårTittel={intro.saksopplysningTittel}
-        grunnlag={intro.utfall === 'OPPFYLT' ? 'Nei' : 'Ja'}
+        vurderingsperiode={vurderingsPeriode}
+        saksopplysningsperiode={saksopplysningsPeriode}
+        kilde={intro.avklartSaksopplysning.kilde}
+        utfall={intro.samletUtfall}
+        vilkårTittel={'Introduksjonsprogrammet'}
+        grunnlag={deltagelseTekst(deltagelse)}
         grunnlagHeader={'Deltar'}
       />
     </VStack>
   );
 };
 
-export default Introduksjonsprogrammet;
+const deltagelseTekst = (deltagelse: Deltagelse): string => {
+  switch (deltagelse) {
+    case Deltagelse.DELTAR:
+      return 'Ja';
+    case Deltagelse.DELTAR_IKKE:
+      return 'Nei';
+  }
+};
+
+export default Kvalifiseringsprogrammet;
