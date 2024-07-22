301ba76210865b8613a011874e307e325135439b
Benedicte Myrvoll <benemyr17@gmail.com>
2024-07-22 16:05:43 +0200

    F Legg til behandlingsdetaljerkomponent

---
+++ src/components/behandlingdetaljer/Behandlingdetaljer.tsx
@@ -0,0 +1,48 @@
+import { VStack, BodyShort } from '@navikt/ds-react';
+import styles from './Behandlingdetaljer.module.css';
+import { useRouter } from 'next/router';
+import { useHentBehandling } from '../../hooks/useHentBehandling';
+import { periodeTilFormatertDatotekst } from '../../utils/date';
+
+const Behandlingdetaljer = () => {
+  const router = useRouter();
+  const behandlingId = router.query.behandlingId as string;
+  const { valgtBehandling, isLoading } = useHentBehandling(behandlingId);
+
+  return (
+    <>
+      <VStack gap="3" className={styles.wrapper}>
+        <BodyShort>
+          <b>Søknadsdato</b>
+        </BodyShort>
+        <BodyShort>
+          {
+            valgtBehandling.kravdatoSaksopplysninger.opprinneligKravdato
+              .kravdato
+          }
+        </BodyShort>
+        <BodyShort>
+          <b>Vurderingsperiode: </b>
+        </BodyShort>
+        <BodyShort>
+          {periodeTilFormatertDatotekst(valgtBehandling.vurderingsperiode)}
+        </BodyShort>
+        <BodyShort>
+          <b>Tiltak</b>
+        </BodyShort>
+        <BodyShort>
+          {
+            valgtBehandling.tiltaksdeltagelsesaksopplysninger
+              .saksopplysninger[0].navn
+          }
+        </BodyShort>
+        <BodyShort>
+          <b>Vurdert av</b>
+        </BodyShort>
+        <BodyShort>{valgtBehandling.saksbehandler}</BodyShort>
+      </VStack>
+    </>
+  );
+};
+
+export default Behandlingdetaljer;
