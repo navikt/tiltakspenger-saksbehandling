deb1356e73240c334ef3e77c74be501857a95d97
Benedicte Myrvoll <benemyr17@gmail.com>
2024-09-19 15:29:22 +0200

    R Fiks navngivning på meldekortuke

--- src/components/meldekort/meldekortside/Meldekort.tsx
+++ src/components/meldekort/meldekortside/Meldekort.tsx
@@ -6,7 +6,7 @@ import { SakContext } from '../../layout/SakLayout';
 import { MeldekortDagDTO } from '../../../types/MeldekortTypes';
 import { useSendMeldekortTilBeslutter } from '../../../hooks/meldekort/useSendMeldekortTilBeslutter';
 import { SubmitHandler, useForm } from 'react-hook-form';
-import Meldekortuke from './Meldekortsuke';
+import Meldekortuke from './Meldekortuke';
 import Varsel from '../../varsel/Varsel';
 import styles from './Meldekort.module.css';
 import { ukeHeading } from '../../../utils/date';
