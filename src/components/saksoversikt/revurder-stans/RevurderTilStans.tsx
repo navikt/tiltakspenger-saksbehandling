8e143396118395c487e16f777f0b53225d8ed4ad
Anders B Nomerstad <anders@nomerstad.com>
2025-01-28 15:32:46 +0100

    Endrer revurder til stans til kun å ta inn fra-dato (+ refactor!)

---
+++ src/components/saksoversikt/revurder-stans/RevurderTilStans.tsx
@@ -0,0 +1,84 @@
+import { Button, HStack } from '@navikt/ds-react';
+import Spørsmålsmodal from '../../revurderingsmodal/Spørsmålsmodal';
+import { Controller, useForm } from 'react-hook-form';
+import { setupValidation } from '../../../utils/validation';
+import Datovelger from '../../revurderingsmodal/Datovelger';
+import { useRef } from 'react';
+import { dateTilISOTekst } from '../../../utils/date';
+import { useOpprettRevurdering } from '../../../hooks/useOpprettRevurdering';
+
+interface RevurderingForm {
+    fraOgMed: Date;
+}
+
+type Props = {
+    førsteLovligeStansdato: string;
+    sisteLovligeStansdato: string;
+    sakId: string;
+    saksnummer: string;
+};
+
+export const RevurderTilStans = ({
+    førsteLovligeStansdato,
+    sisteLovligeStansdato,
+    sakId,
+    saksnummer,
+}: Props) => {
+    const modalRef = useRef(null);
+    const fraOgMed = new Date(førsteLovligeStansdato);
+
+    const { opprettRevurdering } = useOpprettRevurdering(sakId, saksnummer);
+
+    const onSubmit = () => {
+        opprettRevurdering({
+            fraOgMed: dateTilISOTekst(getValues().fraOgMed),
+        });
+    };
+
+    const {
+        getValues,
+        control,
+        formState: { errors },
+        handleSubmit,
+    } = useForm<RevurderingForm>({
+        defaultValues: {
+            fraOgMed,
+        },
+    });
+
+    return (
+        <>
+            <Button size="small" variant="secondary" onClick={() => modalRef.current.showModal()}>
+                Revurder til stans
+            </Button>
+            <Spørsmålsmodal
+                modalRef={modalRef}
+                heading="Velg dato for stans"
+                submitTekst="Opprett revurdering"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                {
+                    <HStack gap="5">
+                        <Controller
+                            name={'fraOgMed'}
+                            control={control}
+                            rules={{
+                                validate: setupValidation([]),
+                            }}
+                            render={({ field: { onChange, value } }) => (
+                                <Datovelger
+                                    onDateChange={onChange}
+                                    label="Stans fra og med"
+                                    minDate={fraOgMed}
+                                    maxDate={new Date(sisteLovligeStansdato)}
+                                    defaultSelected={value}
+                                    errorMessage={errors.fraOgMed ? errors.fraOgMed.message : ''}
+                                />
+                            )}
+                        />
+                    </HStack>
+                }
+            </Spørsmålsmodal>
+        </>
+    );
+};
