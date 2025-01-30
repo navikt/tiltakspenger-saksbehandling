c90bcc8173928450beebe5df6547f04887d09150
Anders B Nomerstad <anders@nomerstad.com>
2025-01-30 15:38:42 +0100

    Tillater ikke å starte behandling av meldeperioder uten rett til tiltakspenger

--- src/components/meldekort/meldekortside/meldekort-behandling/MeldekortBehandlingOpprett.tsx
+++ src/components/meldekort/meldekortside/meldekort-behandling/MeldekortBehandlingOpprett.tsx
@@ -1,8 +1,7 @@
-import { Button, Loader } from '@navikt/ds-react';
-import { MeldeperiodeProps } from '../../../../types/meldekort/Meldeperiode';
+import { Alert, Button, Loader } from '@navikt/ds-react';
+import { MeldeperiodeProps, MeldeperiodeStatus } from '../../../../types/meldekort/Meldeperiode';
 import { useOpprettMeldekortBehandling } from '../../../../hooks/meldekort/useOpprettMeldekortBehandling';
 import { useSak } from '../../../layout/SakLayout';
-import Varsel from '../../../varsel/Varsel';
 import { useHentMeldeperiodeKjede } from '../../../../hooks/meldekort/useHentMeldeperiodeKjede';
 import BekreftelsesModal from '../../../bekreftelsesmodal/BekreftelsesModal';
 import { useRef } from 'react';
@@ -26,10 +25,14 @@ export const MeldekortBehandlingOpprett = ({ meldeperiode }: Props) => {
 
     return (
         <div>
-            <Button onClick={() => modalRef.current.showModal()} className={styles.knapp}>
-                {'Start behandling'}
-            </Button>
-            {feil && <Varsel variant={'error'} melding={feil} className={styles.varsel} />}
+            {meldeperiode.status === MeldeperiodeStatus.IKKE_RETT_TIL_TILTAKSPENGER ? (
+                <Alert variant={'info'}>{'Ikke rett til tiltakspenger for denne perioden'}</Alert>
+            ) : (
+                <Button onClick={() => modalRef.current.showModal()} className={styles.knapp}>
+                    {'Start behandling'}
+                </Button>
+            )}
+            {feil && <Alert variant={'error'}>{feil}</Alert>}
             <BekreftelsesModal
                 modalRef={modalRef}
                 tittel={'Start behandling av meldekortet'}
