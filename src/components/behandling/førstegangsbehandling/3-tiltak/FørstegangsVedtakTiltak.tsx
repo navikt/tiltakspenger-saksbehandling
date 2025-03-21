ffa0f1d944dc174db85f33355200afaa5f34c00c
Tia Firing <tia.firing@nav.no>
2025-03-21 13:38:54 +0100

    skal ikke vises tiltaksdeltakelse-velger før innvilgelse er valgt

--- src/components/behandling/førstegangsbehandling/3-tiltak/FørstegangsVedtakTiltak.tsx
+++ src/components/behandling/førstegangsbehandling/3-tiltak/FørstegangsVedtakTiltak.tsx
@@ -2,24 +2,35 @@ import { VedtakSeksjon } from '../../vedtak-layout/seksjon/VedtakSeksjon';
 import { Alert } from '@navikt/ds-react';
 import { classNames } from '../../../../utils/classNames';
 
-import style from '../4-barn/FørstegangsVedtakBarnetillegg.module.css';
+import style from './FørstegangsVedtakTiltak.module.css';
 import { TiltakPerioder } from './perioder/TiltakPerioder';
+import { useFørstegangsVedtakSkjema } from '../context/FørstegangsVedtakContext';
+import { Separator } from '../../../separator/Separator';
+import { useFørstegangsbehandling } from '../../BehandlingContext';
+import { deltarPaFlereTiltakMedStartOgSluttdato } from '../../../../utils/behandling';
 
 export const FørstegangsVedtakTiltak = () => {
+    const { behandling } = useFørstegangsbehandling();
+    const flereTiltak = deltarPaFlereTiltakMedStartOgSluttdato(behandling);
+    const { resultat } = useFørstegangsVedtakSkjema();
     return (
-        <>
-            <VedtakSeksjon>
-                <VedtakSeksjon.Venstre>
-                    <Alert variant={'warning'} size={'small'}>
-                        Flere tiltak registrert på bruker. Velg tiltak(ene) som bruker skal vurderes
-                        for og periodene som gjelder.
-                    </Alert>
-                </VedtakSeksjon.Venstre>
-            </VedtakSeksjon>
+        flereTiltak && (
+            <div className={classNames(resultat !== 'innvilget' && style.skjult)}>
+                <VedtakSeksjon>
+                    <VedtakSeksjon.Venstre>
+                        <Alert variant={'warning'} size={'small'}>
+                            Flere tiltak registrert på bruker. Velg tiltak(ene) som bruker skal
+                            vurderes for og periodene som gjelder.
+                        </Alert>
+                    </VedtakSeksjon.Venstre>
+                </VedtakSeksjon>
 
-            <VedtakSeksjon className={classNames(style.input)}>
-                <TiltakPerioder />
-            </VedtakSeksjon>
-        </>
+                <VedtakSeksjon className={classNames(style.input)}>
+                    <TiltakPerioder />
+                </VedtakSeksjon>
+
+                <Separator />
+            </div>
+        )
     );
 };
