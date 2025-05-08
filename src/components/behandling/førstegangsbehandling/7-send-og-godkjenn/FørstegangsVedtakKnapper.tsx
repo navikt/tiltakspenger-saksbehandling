efbfa32b18d0dbadf8ab3a0d1e2e8785229956e7
ramziabuqassim <ramzi.abu.qassim@nav.no>
2025-05-08 12:19:53 +0200

    Implement conditional rendering for avslutt behandling button and remove unused components

--- src/components/behandling/førstegangsbehandling/7-send-og-godkjenn/FørstegangsVedtakKnapper.tsx
+++ src/components/behandling/førstegangsbehandling/7-send-og-godkjenn/FørstegangsVedtakKnapper.tsx
@@ -10,10 +10,14 @@ import { førstegangsVedtakValidering } from '../førstegangsVedtakValidering';
 import style from './FørstegangsVedtakKnapper.module.css';
 import { HStack } from '@navikt/ds-react';
 import AvsluttBehandling from '../../../saksoversikt/avsluttBehandling/AvsluttBehandling';
+import { BehandlingStatus } from '../../../../types/BehandlingTypes';
+import { useSaksbehandler } from '../../../../context/saksbehandler/SaksbehandlerContext';
+import router from 'next/router';
 
 export const FørstegangsVedtakKnapper = () => {
     const { behandling } = useFørstegangsbehandling();
     const vedtakSkjema = useFørstegangsVedtakSkjema();
+    const { innloggetSaksbehandler } = useSaksbehandler();
 
     const { sendTilBeslutning, sendTilBeslutningLaster, sendTilBeslutningError } =
         useSendFørstegangsVedtak(behandling, vedtakSkjema);
@@ -21,18 +25,28 @@ export const FørstegangsVedtakKnapper = () => {
     const { godkjennVedtak, godkjennVedtakLaster, godkjennVedtakError } =
         useGodkjennBehandling(behandling);
 
+    const kanAvslutteBehandling =
+        (behandling.status === BehandlingStatus.KLAR_TIL_BEHANDLING ||
+            behandling.status === BehandlingStatus.UNDER_BEHANDLING) &&
+        behandling.avbrutt === null &&
+        behandling.saksbehandler === innloggetSaksbehandler.navIdent;
+
     return (
         <VedtakSeksjon>
             <VedtakSeksjon.Venstre>
                 <HStack justify="space-between" className={style.knapper}>
-                    <AvsluttBehandling
-                        saksnummer={behandling.saksnummer}
-                        behandlingsId={behandling.id}
-                        button={{
-                            size: 'medium',
-                        }}
-                    />
-
+                    {kanAvslutteBehandling && (
+                        <AvsluttBehandling
+                            saksnummer={behandling.saksnummer}
+                            behandlingsId={behandling.id}
+                            button={{
+                                size: 'medium',
+                            }}
+                            onSuccess={() => {
+                                router.push(`/sak/${behandling.saksnummer}`);
+                            }}
+                        />
+                    )}
                     <BehandlingSendTilBeslutning
                         send={sendTilBeslutning}
                         laster={sendTilBeslutningLaster}
