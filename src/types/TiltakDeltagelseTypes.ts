ff0acc5136e5e2d5c4f64459bcafde067c954601
tiafiring <tia.firing@nav.no>
2025-10-28 08:45:58 +0100

    1694 viser hvis deltakelsesprosent kommer fra gjennomføringen (#725)

--- src/types/TiltakDeltagelseTypes.ts
+++ src/types/TiltakDeltagelseTypes.ts
@@ -16,6 +16,7 @@ export type Tiltaksdeltagelse = {
     deltakelseProsent: number | null;
     antallDagerPerUke: number | null;
     kilde: Kilde;
+    deltakelseProsentFraGjennomforing: boolean | null;
 };
 
 export type TiltaksdeltagelseMedPeriode = Tiltaksdeltagelse & {
