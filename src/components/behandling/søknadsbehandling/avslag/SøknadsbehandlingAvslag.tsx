43d6fc1aaa7752dcc40d69ea85266264483f4abd
Anders Nomerstad <anders@nomerstad.com>
2025-10-29 09:21:57 +0100

    Fjerner ubrukte filer

--- src/components/behandling/søknadsbehandling/avslag/SøknadsbehandlingAvslag.tsx
+++
@@ -1,5 +0,0 @@
-import { SøknadsbehandlingAvslagsgrunner } from '~/components/behandling/søknadsbehandling/6-avslagsgrunner/SøknadsbehandlingAvslagsgrunner';
-
-export const SøknadsbehandlingAvslag = () => {
-    return <SøknadsbehandlingAvslagsgrunner />;
-};
