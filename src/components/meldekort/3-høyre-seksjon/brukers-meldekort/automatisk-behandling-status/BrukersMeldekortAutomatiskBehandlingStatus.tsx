d883cb01249632b8bdcc82b1a5f5c332269101fd
Anders Nomerstad <anders@nomerstad.com>
2025-11-10 20:30:52 +0100

    Bedre varsel-meldinger når automatisk meldekortbehandling har stoppet opp

--- src/components/meldekort/3-høyre-seksjon/brukers-meldekort/automatisk-behandling-status/BrukersMeldekortAutomatiskBehandlingStatus.tsx
+++ src/components/meldekort/3-høyre-seksjon/brukers-meldekort/automatisk-behandling-status/BrukersMeldekortAutomatiskBehandlingStatus.tsx
@@ -7,6 +7,7 @@ enum MetaStatus {
     Venter = 'Venter',
     SkalIkkeBehandles = 'SkalIkkeBehandles',
     MeldekortFeil = 'MeldekortFeil',
+    SakFeil = 'SakFeil',
     TekniskFeil = 'Teknisk',
 }
 
@@ -33,26 +34,37 @@ const alertVariant: Record<MetaStatus, ComponentProps<typeof Alert>['variant']> 
     [MetaStatus.Venter]: 'info',
     [MetaStatus.SkalIkkeBehandles]: 'info',
     [MetaStatus.MeldekortFeil]: 'warning',
+    [MetaStatus.SakFeil]: 'warning',
     [MetaStatus.TekniskFeil]: 'error',
 } as const;
 
 const tilMetaTekst: { [key in MetaStatus]?: string } = {
     [MetaStatus.SkalIkkeBehandles]: 'Behandles ikke automatisk',
-    [MetaStatus.MeldekortFeil]: 'Tilstand på meldekort/sak stoppet automatisk behandling',
+    [MetaStatus.MeldekortFeil]: 'Tilstand på meldekortet stoppet automatisk behandling',
+    [MetaStatus.SakFeil]: 'Tilstand på saken stoppet automatisk behandling',
     [MetaStatus.TekniskFeil]: 'Teknisk feil ved automatisk behandling',
 } as const;
 
 const tilMetaStatus: Record<MeldekortBehandletAutomatiskStatus, MetaStatus> = {
     [MeldekortBehandletAutomatiskStatus.BEHANDLET]: MetaStatus.Behandlet,
+
     [MeldekortBehandletAutomatiskStatus.VENTER_BEHANDLING]: MetaStatus.Venter,
+
     [MeldekortBehandletAutomatiskStatus.ALLEREDE_BEHANDLET]: MetaStatus.SkalIkkeBehandles,
     [MeldekortBehandletAutomatiskStatus.SKAL_IKKE_BEHANDLES_AUTOMATISK]:
         MetaStatus.SkalIkkeBehandles,
+
     [MeldekortBehandletAutomatiskStatus.UTDATERT_MELDEPERIODE]: MetaStatus.MeldekortFeil,
-    [MeldekortBehandletAutomatiskStatus.ER_UNDER_REVURDERING]: MetaStatus.MeldekortFeil,
     [MeldekortBehandletAutomatiskStatus.FOR_MANGE_DAGER_REGISTRERT]: MetaStatus.MeldekortFeil,
     [MeldekortBehandletAutomatiskStatus.KAN_IKKE_MELDE_HELG]: MetaStatus.MeldekortFeil,
     [MeldekortBehandletAutomatiskStatus.FOR_MANGE_DAGER_GODKJENT_FRAVÆR]: MetaStatus.MeldekortFeil,
+    [MeldekortBehandletAutomatiskStatus.INGEN_DAGER_GIR_RETT]: MetaStatus.MeldekortFeil,
+
+    [MeldekortBehandletAutomatiskStatus.ER_UNDER_REVURDERING]: MetaStatus.SakFeil,
+    [MeldekortBehandletAutomatiskStatus.HAR_ÅPEN_BEHANDLING]: MetaStatus.SakFeil,
+    [MeldekortBehandletAutomatiskStatus.MÅ_BEHANDLE_FØRSTE_KJEDE]: MetaStatus.SakFeil,
+    [MeldekortBehandletAutomatiskStatus.MÅ_BEHANDLE_NESTE_KJEDE]: MetaStatus.SakFeil,
+
     [MeldekortBehandletAutomatiskStatus.HENTE_NAVKONTOR_FEILET]: MetaStatus.TekniskFeil,
     [MeldekortBehandletAutomatiskStatus.BEHANDLING_FEILET_PÅ_SAK]: MetaStatus.TekniskFeil,
     [MeldekortBehandletAutomatiskStatus.UTBETALING_FEILET_PÅ_SAK]: MetaStatus.TekniskFeil,
@@ -79,4 +91,12 @@ const tilStatusTekst: Record<MeldekortBehandletAutomatiskStatus, string> = {
     [MeldekortBehandletAutomatiskStatus.UTBETALING_FEILET_PÅ_SAK]:
         'Feil relatert til beregning av utbetalingen',
     [MeldekortBehandletAutomatiskStatus.UKJENT_FEIL]: 'Ukjent feil',
+    [MeldekortBehandletAutomatiskStatus.HAR_ÅPEN_BEHANDLING]:
+        'Det fantes allerede en åpen meldekortbehandling - vi tillater kun en samtidig åpen behandling',
+    [MeldekortBehandletAutomatiskStatus.MÅ_BEHANDLE_FØRSTE_KJEDE]:
+        'Første meldeperiode må alltid behandles først',
+    [MeldekortBehandletAutomatiskStatus.MÅ_BEHANDLE_NESTE_KJEDE]:
+        'Forrige meldeperiode var ikke behandlet - periodene må alltid behandles i kronologisk rekkefølge',
+    [MeldekortBehandletAutomatiskStatus.INGEN_DAGER_GIR_RETT]:
+        'Ingen dager gir rett i denne meldeperioden',
 } as const;
