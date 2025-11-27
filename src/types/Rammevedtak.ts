d35680622ca89170ae46660ba761f8665a213268
John Andre Hestad <john.andre.hestad@nav.no>
2025-11-27 12:49:45 +0100

    Bruk oppdaterte felter fra rammevedtak-typen (#754)

--- src/types/Rammevedtak.ts
+++ src/types/Rammevedtak.ts
@@ -11,15 +11,21 @@ export type Rammevedtak = {
     opprettet: string;
     vedtaksdato: Nullable<string>;
     resultat: RammebehandlingResultat;
-    periode: Periode;
-    // Perioden der vedtaket fortsatt er gjeldende for sakens nå-tilstand
-    gjeldendePeriode: Periode;
+    // Vedtaksperioden da den ble vedtatt. Er ikke sikkert den er gjeldende lenger, hvis den har blitt omgjort. Avslagsvedtak er aldri gjeldende.
+    opprinneligVedtaksperiode: Periode;
+    // Vil alltid være tom for avslag, stans og rene opphør. For innvilgelser (inkl. omgjøring) og forlengelser vil dette være perioden(e) som opprinnelig ble innvilget i vedtaket.
+    opprinneligInnvilgetPerioder: Periode[];
+    // Listen over perioder der vedtaket fortsatt er gjeldende for sakens nå-tilstand. Den var opprinnelig en hel periode, men kan ha blitt splittet av en eller flere omgjøringer. Vil alltid være tom for avslag siden de aldri er gjeldende.
+    gjeldendeVedtaksperioder: Periode[];
+    // Vil alltid være tom for avslag, stans og rene opphør. For innvilgelser (inkl. omgjøring) og forlengelser vil dette være perioden(e) som fortsatt er innvilget i vedtaket for sakens nå-tilstand.
+    gjeldendeInnvilgetPerioder: Periode[];
     saksbehandler: string;
     beslutter: string;
     // TODO jah: Denne blir en periodisering. På samme måte som behandlingen. Brukes for å vises i tidslinja.
     antallDagerPerMeldeperiode: number;
     // Inkluderer perioder med 0 barn
     barnetillegg: Nullable<Barnetillegg>;
+    erGjeldende: boolean;
 };
 
 export type RammevedtakMedBehandling = Rammevedtak & { behandling: Rammebehandling };
