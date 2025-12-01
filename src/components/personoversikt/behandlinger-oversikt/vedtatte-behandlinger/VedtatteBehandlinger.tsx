8cced1bd13fb776dd3bd4267c2755c77816bd06a
Anders Nomerstad <anders@nomerstad.com>
2025-12-01 11:58:56 +0100

    Kan filtrere vekk omgjorte vedtak i personoversikten. Indikerer omgjøringsgrade med bakgrunnsfarge

--- src/components/personoversikt/behandlinger-oversikt/vedtatte-behandlinger/VedtatteBehandlinger.tsx
+++ src/components/personoversikt/behandlinger-oversikt/vedtatte-behandlinger/VedtatteBehandlinger.tsx
@@ -1,18 +1,31 @@
-import { Alert, Heading, VStack } from '@navikt/ds-react';
+import { Alert, Checkbox, CheckboxGroup, Heading, HStack, VStack } from '@navikt/ds-react';
 import { VedtatteBehandlingerTabell } from './VedtatteBehandlingerTabell';
 import { Rammebehandling } from '~/types/Rammebehandling';
 import { SakId } from '~/types/Sak';
-import { Rammevedtak, RammevedtakMedBehandling } from '~/types/Rammevedtak';
+import { Omgjøringsgrad, Rammevedtak, RammevedtakMedBehandling } from '~/types/Rammevedtak';
+import { useState } from 'react';
+import { classNames } from '~/utils/classNames';
 
-import styles from '../../Personoversikt.module.css';
+import style from './VedtatteBehandlinger.module.css';
 
 type Props = {
     sakId: SakId;
     behandlinger: Rammebehandling[];
     alleRammevedtak: Rammevedtak[];
+    className?: string;
 };
 
-export const VedtatteBehandlinger = ({ sakId, behandlinger, alleRammevedtak }: Props) => {
+export const VedtatteBehandlinger = ({
+    sakId,
+    behandlinger,
+    alleRammevedtak,
+    className,
+}: Props) => {
+    const [visOmgjorte, setVisOmgjorte] = useState<Omgjøringsgrad[]>([
+        Omgjøringsgrad.HELT,
+        Omgjøringsgrad.DELVIS,
+    ]);
+
     if (alleRammevedtak.length === 0) {
         return null;
     }
@@ -31,11 +44,40 @@ export const VedtatteBehandlinger = ({ sakId, behandlinger, alleRammevedtak }: P
 
     const antallVedtakUtenBehandling = alleRammevedtak.length - vedtakMedBehandling.length;
 
+    const vedtakSomSkalVises = vedtakMedBehandling.filter(
+        (vedtak) => !vedtak.omgjortGrad || visOmgjorte.includes(vedtak.omgjortGrad),
+    );
+
     return (
-        <VStack className={styles.tabellwrapper} gap={'2'}>
-            <Heading level="3" size="small">
-                {'Vedtatte behandlinger'}
-            </Heading>
+        <VStack className={className} gap={'2'}>
+            <HStack justify={'space-between'}>
+                <Heading level="3" size="small">
+                    {'Vedtatte behandlinger'}
+                </Heading>
+                <CheckboxGroup
+                    legend={'Vis omgjorte vedtak'}
+                    hideLegend={true}
+                    size={'small'}
+                    value={visOmgjorte}
+                    onChange={(values: Omgjøringsgrad[]) => {
+                        setVisOmgjorte(values);
+                    }}
+                    className={style.omgjortGroup}
+                >
+                    <Checkbox
+                        value={Omgjøringsgrad.HELT}
+                        className={classNames(style.omgjortCheckbox, style.heltOmgjortBg)}
+                    >
+                        {'Helt omgjort'}
+                    </Checkbox>
+                    <Checkbox
+                        value={Omgjøringsgrad.DELVIS}
+                        className={classNames(style.omgjortCheckbox, style.delvisOmgjortBg)}
+                    >
+                        {'Delvis omgjort'}
+                    </Checkbox>
+                </CheckboxGroup>
+            </HStack>
             {antallVedtakUtenBehandling > 0 && (
                 <Alert
                     variant={'error'}
@@ -43,7 +85,7 @@ export const VedtatteBehandlinger = ({ sakId, behandlinger, alleRammevedtak }: P
             )}
             <VedtatteBehandlingerTabell
                 sakId={sakId}
-                rammevedtakMedBehandlinger={vedtakMedBehandling}
+                rammevedtakMedBehandlinger={vedtakSomSkalVises}
             />
         </VStack>
     );
