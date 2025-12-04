4460fdb840334d01119dd1e85ac7c239cedcb2a8
Anders Nomerstad <anders@nomerstad.com>
2025-12-04 17:40:19 +0100

    Bruker generiske funksjoner for re-periodisering ved endring av innvilgelsesperiode

--- src/components/behandling/context/innvilgelse/slices/innvilgelsesperiodeContext.ts
+++ src/components/behandling/context/innvilgelse/slices/innvilgelsesperiodeContext.ts
@@ -1,6 +1,5 @@
-import { MedPeriode, Periode } from '~/types/Periode';
-import { erFullstendigPeriode, perioderOverlapper } from '~/utils/periode';
-import { datoMax, datoMin } from '~/utils/date';
+import { Periode } from '~/types/Periode';
+import { erFullstendigPeriode, krympPeriodisering, utvidPeriodisering } from '~/utils/periode';
 import { InnvilgelseState } from '~/components/behandling/context/innvilgelse/innvilgelseContext';
 import { Reducer } from 'react';
 import { Rammebehandling } from '~/types/Rammebehandling';
@@ -38,81 +37,16 @@ export const innvilgelsesperiodeReducer: Reducer<InnvilgelseState, Innvilgelsesp
         ...state,
         innvilgelsesperiode: nyInnvilgelsesperiode,
 
-        valgteTiltaksdeltakelser: oppdaterPeriodisering(
+        valgteTiltaksdeltakelser: utvidPeriodisering(
             state.valgteTiltaksdeltakelser,
             nyInnvilgelsesperiode,
-            true,
         ),
 
-        antallDagerPerMeldeperiode: oppdaterPeriodisering(
+        antallDagerPerMeldeperiode: utvidPeriodisering(
             state.antallDagerPerMeldeperiode,
             nyInnvilgelsesperiode,
-            true,
         ),
 
-        barnetilleggPerioder: oppdaterPeriodisering(
-            state.barnetilleggPerioder,
-            nyInnvilgelsesperiode,
-            false,
-        ),
+        barnetilleggPerioder: krympPeriodisering(state.barnetilleggPerioder, nyInnvilgelsesperiode),
     };
-};
-
-// Denne må kanskje tweakes litt hvis vi får periodiserte innvilgelser i en behandling
-const oppdaterPeriodisering = <T extends MedPeriode>(
-    periodisering: T[],
-    innvilgelsesperiode: Periode,
-    // Tiltaksdeltagelse og antall dager per meldeperiode skal alltid fylle hele innvilgelsesperioden ved innvilgelse,
-    // men barnetillegg kan omfatte kun deler av innvilgelsesperioden. Vi gjør en best-effort for å tilpasse for dette.
-    skalAlltidFylleInnvilgelsesperioden: boolean,
-): T[] => {
-    const perioderInnenforInnvilgelsesperioden = periodisering.filter((p) =>
-        perioderOverlapper(innvilgelsesperiode, p.periode),
-    );
-
-    if (perioderInnenforInnvilgelsesperioden.length === 0) {
-        return [];
-    }
-
-    const førstePeriode = perioderInnenforInnvilgelsesperioden.at(0)!;
-
-    if (perioderInnenforInnvilgelsesperioden.length === 1) {
-        return perioderInnenforInnvilgelsesperioden.with(0, {
-            ...førstePeriode,
-            periode: skalAlltidFylleInnvilgelsesperioden
-                ? innvilgelsesperiode
-                : {
-                      fraOgMed: datoMax(
-                          innvilgelsesperiode.fraOgMed,
-                          førstePeriode.periode.fraOgMed,
-                      ),
-                      tilOgMed: datoMin(
-                          innvilgelsesperiode.tilOgMed,
-                          førstePeriode.periode.tilOgMed,
-                      ),
-                  },
-        });
-    }
-
-    const sistePeriode = perioderInnenforInnvilgelsesperioden.at(-1)!;
-
-    return perioderInnenforInnvilgelsesperioden
-        .with(0, {
-            ...førstePeriode,
-            periode: {
-                fraOgMed: skalAlltidFylleInnvilgelsesperioden
-                    ? innvilgelsesperiode.fraOgMed
-                    : datoMax(innvilgelsesperiode.fraOgMed, førstePeriode.periode.fraOgMed),
-                tilOgMed: førstePeriode.periode.tilOgMed,
-            },
-        })
-        .with(-1, {
-            ...sistePeriode,
-            periode: {
-                fraOgMed: sistePeriode.periode.fraOgMed,
-                tilOgMed: skalAlltidFylleInnvilgelsesperioden
-                    ? innvilgelsesperiode.tilOgMed
-                    : datoMin(innvilgelsesperiode.tilOgMed, sistePeriode.periode.tilOgMed),
-            },
-        });
 };
