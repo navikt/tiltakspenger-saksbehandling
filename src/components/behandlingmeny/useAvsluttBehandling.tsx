863be945c7c6c7f5ecfeb0357e1b9d3f8d1f4d0d
Anders Nomerstad <anders@nomerstad.com>
2026-01-27 17:28:51 +0100

    Fjerner type for avslutt søknad DTO (ikke i bruk)

--- src/components/behandlingmeny/useAvsluttBehandling.tsx
+++ src/components/behandlingmeny/useAvsluttBehandling.tsx
@@ -1,15 +1,11 @@
 import { useFetchJsonFraApi } from '~/utils/fetch/useFetchFraApi';
 import { SakProps } from '~/types/Sak';
-import { SøknadId } from '~/types/Søknad';
 import { BehandlingId } from '~/types/Rammebehandling';
-
-export type SøknadIdEllerBehandlingId =
-    | { søknadId: SøknadId; behandlingId?: never }
-    | { behandlingId: BehandlingId; søknadId?: never };
 
 type AvsluttBehandlingDTO = {
     begrunnelse: string;
-} & SøknadIdEllerBehandlingId;
+    behandlingId: BehandlingId;
+};
 
 export const useAvsluttBehandling = (saksnummer: string) => {
     const {
