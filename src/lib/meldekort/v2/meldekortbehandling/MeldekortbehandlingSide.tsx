28fb0588659ba81b80f2375a1c86b8c2de0b082f
Anders Nomerstad <anders@nomerstad.com>
2026-06-02 19:09:30 +0200

    Kan velge steg i meldekortbehandlingen

--- src/lib/meldekort/v2/meldekortbehandling/MeldekortbehandlingSide.tsx
+++ src/lib/meldekort/v2/meldekortbehandling/MeldekortbehandlingSide.tsx
@@ -1,11 +1,13 @@
 import { PersonaliaHeader } from '~/lib/personaliaheader/PersonaliaHeader';
 import { useSak } from '~/lib/sak/SakContext';
-import { VStack } from '@navikt/ds-react';
+import { Stepper, VStack } from '@navikt/ds-react';
 import { MeldekortbehandlingV2Provider } from '~/lib/meldekort/v2/meldekortbehandling/context/MeldekortbehandlingV2Context';
 import { Meldeperiodebehandlinger } from '~/lib/meldekort/v2/meldekortbehandling/meldeperioder/Meldeperiodebehandlinger';
 import { MeldekortbehandlingHeader } from '~/lib/meldekort/v2/meldekortbehandling/header/MeldekortbehandlingHeader';
-import { MeldekortbehandlingHandlinger } from '~/lib/meldekort/v2/meldekortbehandling/handlinger/MeldekortbehandlingHandlinger';
 import { MeldekortbehandlingPropsV2 } from '~/lib/meldekort/v2/typer';
+import { useState } from 'react';
+import { InfokortEnkel } from '~/lib/_felles/infokort/InfokortEnkel';
+import { Separator } from '~/lib/_felles/separator/Separator';
 
 import style from './MeldekortbehandlingSide.module.css';
 
@@ -15,6 +17,7 @@ type Props = {
 
 export const MeldekortbehandlingSide = ({ meldekortbehandling }: Props) => {
     const { sakId, saksnummer } = useSak().sak;
+    const [aktivtSteg, setAktivtSteg] = useState(1);
 
     return (
         <MeldekortbehandlingV2Provider meldekortbehandling={meldekortbehandling}>
@@ -22,9 +25,38 @@ export const MeldekortbehandlingSide = ({ meldekortbehandling }: Props) => {
 
             <VStack className={style.meldekortbehandling}>
                 <MeldekortbehandlingHeader />
-                <Meldeperiodebehandlinger />
-                <MeldekortbehandlingHandlinger />
+
+                <Stepper
+                    activeStep={aktivtSteg}
+                    onStepChange={setAktivtSteg}
+                    orientation={'horizontal'}
+                    className={style.stepper}
+                >
+                    <Stepper.Step href="#">{'Meldeperioder'}</Stepper.Step>
+                    <Stepper.Step href="#">{'Beregning og simulering'}</Stepper.Step>
+                    <Stepper.Step href="#">{'Begrunnelse og brev'}</Stepper.Step>
+                    <Stepper.Step href="#">{'Valider og send inn'}</Stepper.Step>
+                </Stepper>
+
+                <Separator />
+
+                <ComponentForStep steg={aktivtSteg} />
             </VStack>
         </MeldekortbehandlingV2Provider>
     );
+};
+
+const ComponentForStep = ({ steg }: { steg: number }) => {
+    switch (steg) {
+        case 1:
+            return <Meldeperiodebehandlinger />;
+        case 2:
+            return 'Beregning og simulering goes here!';
+        case 3:
+            return 'Begrunnelse og brev goes here!';
+        case 4:
+            return 'Validering og innsending goes here!';
+    }
+
+    return <InfokortEnkel data-color={'danger'}>{`Ugyldig steg: ${steg}`}</InfokortEnkel>;
 };
