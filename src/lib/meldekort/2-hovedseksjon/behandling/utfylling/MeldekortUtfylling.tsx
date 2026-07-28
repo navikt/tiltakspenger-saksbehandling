f2ecfed31c4deb7d7c4cb56fca643cc4cccedfcd
ramziabuqassim <ramzi.abu.qassim@nav.no>
2026-07-28 11:39:32 +0200

    kun 1 brev hentes fra meldekortvedtaks pdf

--- src/lib/meldekort/2-hovedseksjon/behandling/utfylling/MeldekortUtfylling.tsx
+++ src/lib/meldekort/2-hovedseksjon/behandling/utfylling/MeldekortUtfylling.tsx
@@ -28,7 +28,7 @@ import AvsluttMeldekortbehandling from '~/lib/personoversikt/meldekort-oversikt/
 import { meldeperiodeUrl, personoversiktUrl } from '~/utils/urls';
 import { MeldekortBeregningOgSimulering } from '~/lib/meldekort/0-felles-komponenter/beregning-simulering/MeldekortBeregningOgSimulering';
 import Divider from '~/lib/_felles/divider/Divider';
-import { useFetchResponseFromApi } from '~/utils/fetch/useFetchFraApi';
+import { useFetchBlobFraApi } from '~/utils/fetch/useFetchFraApi';
 import { hookFormErrorsTilFeiloppsummering } from '~/utils/validering';
 import { Nullable } from '~/types/UtilTypes';
 import { useNotification } from '~/lib/_felles/notifications/NotificationContext';
@@ -51,7 +51,6 @@ import {
 
 import styles from './MeldekortUtfylling.module.css';
 import { ForhåndsvisMeldekortbehandlingBrevBody } from '~/lib/meldekort/v2/meldekortbehandling/fritekst-og-innsending/begrunnelse-og-brev/forhåndsvis-brev/MeldekortbehandlingForhåndsvisBrev';
-import { parseMultipartPdfs } from '~/utils/fetch/multipartPdf';
 
 type Props = {
     meldekortbehandling: MeldekortbehandlingProps;
@@ -93,25 +92,9 @@ export const MeldekortUtfylling = ({ meldekortbehandling }: Props) => {
         .dager.every((dag) => dag.status !== MeldekortbehandlingDagStatus.IkkeBesvart);
     const skalViseBeregningVarsel = skjemaErEndret && skjemaErUtfylt;
 
-    const forhåndsvisDobbelBrev = useFetchResponseFromApi<ForhåndsvisMeldekortbehandlingBrevBody>(
+    const forhåndsvisBrev = useFetchBlobFraApi<ForhåndsvisMeldekortbehandlingBrevBody>(
         `/sak/${sakId}/meldekortbehandling/${meldekortbehandlingId}/forhandsvis`,
         'POST',
-        {
-            onSuccess: async (response) => {
-                const contentType = response.headers.get('content-type');
-
-                if (contentType?.includes('multipart')) {
-                    const pdfs = await parseMultipartPdfs(response);
-                    pdfs.forEach((pdfBlob) => {
-                        window.open(URL.createObjectURL(pdfBlob));
-                    });
-                } else {
-                    response.blob().then((blob) => {
-                        window.open(URL.createObjectURL(blob));
-                    });
-                }
-            },
-        },
     );
 
     const oppdaterMeldekortbehandling = useOppdaterMeldekortbehandling({
@@ -146,7 +129,7 @@ export const MeldekortUtfylling = ({ meldekortbehandling }: Props) => {
                 break;
             case 'lagreOgBeregn':
                 //fordi brevet krever at beregning gjøres for å forhåndsvise, fjerner vi tidligere feil som kan ha oppstått
-                forhåndsvisDobbelBrev.reset();
+                forhåndsvisBrev.reset();
                 oppdaterMeldekortbehandling.reset();
                 oppdaterMeldekortbehandling.trigger(meldekortbehandlingFormTilDto(data, kjedeId));
                 break;
@@ -250,12 +233,12 @@ export const MeldekortUtfylling = ({ meldekortbehandling }: Props) => {
                         type="button"
                         variant="secondary"
                         size="small"
-                        loading={forhåndsvisDobbelBrev.isMutating}
+                        loading={forhåndsvisBrev.isMutating}
                         disabled={meldekortbehandling.erAvsluttet || !skalSendeVedtaksbrev}
                         onClick={() => {
                             //resetter eventuelle tidligere feil før ny request
-                            forhåndsvisDobbelBrev.reset();
-                            forhåndsvisDobbelBrev.trigger(
+                            forhåndsvisBrev.reset();
+                            forhåndsvisBrev.trigger(
                                 {
                                     tekstTilVedtaksbrev: formContext.getValues(
                                         'tekstTilVedtaksbrev',
@@ -269,32 +252,16 @@ export const MeldekortUtfylling = ({ meldekortbehandling }: Props) => {
                                         },
                                     ],
                                 },
-                                {
-                                    //litt dobelt opp siden dette er kun midlertidig
-                                    onSuccess: async (response) => {
-                                        const contentType = response.headers.get('content-type');
-
-                                        if (contentType?.includes('multipart')) {
-                                            const pdfs = await parseMultipartPdfs(response);
-                                            pdfs.forEach((pdfBlob) => {
-                                                window.open(URL.createObjectURL(pdfBlob));
-                                            });
-                                        } else {
-                                            response.blob().then((blob) => {
-                                                window.open(URL.createObjectURL(blob));
-                                            });
-                                        }
-                                    },
-                                },
+                                { onSuccess: (blob) => window.open(URL.createObjectURL(blob!)) },
                             );
                         }}
                     >
                         Forhåndsvis brev
                     </Button>
-                    {forhåndsvisDobbelBrev.error && (
+                    {forhåndsvisBrev.error && (
                         <Alert variant="error" size="small">
                             <BodyShort>Feil ved forhåndsvisning av brev</BodyShort>
-                            <BodyShort>{forhåndsvisDobbelBrev.error.message}</BodyShort>
+                            <BodyShort>{forhåndsvisBrev.error.message}</BodyShort>
                         </Alert>
                     )}
                     <Divider orientation="horizontal" />
