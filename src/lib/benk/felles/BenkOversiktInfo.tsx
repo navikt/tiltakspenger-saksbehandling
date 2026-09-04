7496e1d89a88c983c36dcc325fa3f944647d1b38
Anders Nomerstad <anders@nomerstad.com>
2026-09-04 18:15:56 +0200

    Tweak visning av antall behandlinger

--- src/lib/benk/felles/BenkOversiktInfo.tsx
+++ src/lib/benk/felles/BenkOversiktInfo.tsx
@@ -2,18 +2,11 @@ import { BodyShort, InlineMessage, VStack } from '@navikt/ds-react';
 import { BenkOversikt } from '../typer/felles';
 
 export const BenkOversiktInfo = ({ oversikt }: { oversikt: BenkOversikt<unknown> }) => {
-    const { totalAntall, totalAntallUfiltrert, antallFiltrertPgaTilgang } = oversikt;
-
-    const antallFiltrertAvFiltervalg = totalAntallUfiltrert - totalAntall;
+    const { behandlinger, totalAntall, totalAntallUfiltrert, antallFiltrertPgaTilgang } = oversikt;
 
     return (
         <VStack gap={'space-4'}>
-            <BodyShort>{`Viser ${totalAntall} av ${totalAntallUfiltrert} behandlinger`}</BodyShort>
-            {antallFiltrertAvFiltervalg > 0 && (
-                <InlineMessage status={'info'} size={'small'}>
-                    {`${totalAntall} treff med valgte filtre - ${antallFiltrertAvFiltervalg} filtrert vekk`}
-                </InlineMessage>
-            )}
+            <BodyShort>{`Viser ${totalAntall} av totalt ${totalAntallUfiltrert} behandlinger med valgte filtre (${behandlinger.length} på denne siden)`}</BodyShort>
             {antallFiltrertPgaTilgang > 0 && (
                 <InlineMessage status={'warning'} size={'small'}>
                     {`${antallFiltrertPgaTilgang} filtrert vekk pga manglende tilgang`}
