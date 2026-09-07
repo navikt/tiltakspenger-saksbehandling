e93f11bac5900c575a6536e2fe216e339fea8497
Anders Nomerstad <anders@nomerstad.com>
2026-09-07 10:13:18 +0200

    Refactor

--- src/lib/benk/felles/BenkPanel.tsx
+++ src/lib/benk/felles/BenkPanel.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
-import { HStack, VStack } from '@navikt/ds-react';
+import { BodyShort, HStack, InlineMessage, VStack } from '@navikt/ds-react';
 import { BenkOversikt } from '../typer/felles';
-import { BenkOversiktInfo } from './BenkOversiktInfo';
 import { BenkPaginering } from './BenkPaginering';
 
 type Props = {
@@ -11,16 +10,34 @@ type Props = {
 };
 
 /** Oppbygningen alle fanene deler: filter øverst, så tellinger, så tabellen med paginering over og under */
-export const BenkPanel = ({ oversikt, filter, tabell }: Props) => (
-    <VStack gap={'space-16'}>
-        {filter}
-        <BenkOversiktInfo oversikt={oversikt} />
-        <HStack justify={'center'}>
-            <BenkPaginering oversikt={oversikt} />
-        </HStack>
-        {tabell}
-        <HStack justify={'center'}>
-            <BenkPaginering oversikt={oversikt} />
-        </HStack>
-    </VStack>
-);
+export const BenkPanel = ({ oversikt, filter, tabell }: Props) => {
+    const { behandlinger, totalAntall, totalAntallUfiltrert, antallFiltrertPgaTilgang } = oversikt;
+
+    return (
+        <VStack gap={'space-16'}>
+            {filter}
+
+            <VStack gap={'space-4'}>
+                <BodyShort>
+                    {`Viser ${totalAntall} av totalt ${totalAntallUfiltrert} behandlinger med valgte filtre (${behandlinger.length} på denne siden)`}
+                </BodyShort>
+
+                {antallFiltrertPgaTilgang > 0 && (
+                    <InlineMessage status={'warning'} size={'small'}>
+                        {`${antallFiltrertPgaTilgang} filtrert vekk pga manglende tilgang`}
+                    </InlineMessage>
+                )}
+            </VStack>
+
+            <HStack justify={'center'}>
+                <BenkPaginering oversikt={oversikt} />
+            </HStack>
+
+            {tabell}
+
+            <HStack justify={'center'}>
+                <BenkPaginering oversikt={oversikt} />
+            </HStack>
+        </VStack>
+    );
+};
